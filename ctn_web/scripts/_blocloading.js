1e5c7348ab255d5bac62b773f4ece6281436a14d
chithanhnguyen.math <chithanhnguyen.math@gmail.com>
2011-07-15 16:45:19 +0000

    UP : add home page and its style

--- ctn_web/scripts/_blocloading.js
+++ ctn_web/scripts/_blocloading.js
@@ -8,20 +8,29 @@ $(document).ready(function() {
 });
 
 $(document).ready(function() {
-	$("div#content").load("html_fr/_cv_fr.html  #content  div");
-	$("#contentstyle").attr("href", "styles/_cv_style_1.css");
+	$("div#content").load("html_fr/_home_fr.html  #content  div");
+	$("#contentstyle").attr("href", "styles/_home_style_1.css");
 
 
 	$("div#sidebar").load("html_fr/_sidebar_fr.html  #sidebar  div");
 	$("#sidebarstyle").attr("href", "styles/_sidebar_style_1.css");
 	
 
-	$("div#footer").html("<h1>" +"TOTO XML" +"</h1>");
+	$("div#footer").html("<h1>" +"Here putting footer of page" +"</h1>");
 	
-	var xml = $.load("toto.xml");
 	
 /*	 		_cv_html = $.parseXML( 'toto.xml' );
 	 _cv_content = _cv_html.find("content");
 	 $( "div#sidebar" ).append( $_cv_content.text() ); */
 
 });
+
+
+function _load_content(lang, htmlfilepath, stylefilepath ){
+	//loading a content must load also its style
+	
+	//styles folder 
+	//lang give the html_lang   folder
+	
+	
+}
