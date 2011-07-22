1c00b94e4d2c1c7e4f26d3bda51a3d9a15ffd30a
chithanhnguyen.math@gmail.com <chithanhnguyen.math@gmail.com@d822209c-5b66-c3cb-24dc-bd16cd606af5>
2011-07-22 18:40:23 +0000

    UP : change ordre of loading js file because of dependency of functions

--- trunk/ctn_web/scripts/_layout.js
+++ trunk/ctn_web/scripts/_layout.js
@@ -12,18 +12,26 @@
 
 function resetLayout()
 {
-	$('#page-header').css({"height":"100px"});
+	$('#page-header').css({
+		"height":"100px"
+		,"background" : "#FFFFFF url(images/top_wrapper.jpg) repeat-x"
+			});
 	$('#page').css({"width":" 70em"});
 	$('#sidebar').css({"width":" 33%"});
 	$('#content').css({"width":" 63%"});
-	$('#page-footer').css({"height":"100px"});
+	$('#page-footer').css({
+		"height":"100px"
+		,"background" : "#FFFFFF url(images/bottom_wrapper.jpg) repeat-x"
+			});
+	_debug_layout();
 }
 
 
 function _debug_layout()
 {
 	//plot border of all bloc
-	$('body').css({"border":"1px solid #F0F8FF"});
+	//$('body').css({"border":"1px solid #ffffff"});
+	$('#whole-page').css({"border":"1px solid #00FF00"});
 	$('#page-header').css({"border":"1px solid #D2691E"});
 	$('#top-page').css({"border":"1px solid #808080"});
 	$('#page').css({"border":"1px solid #008000"});
