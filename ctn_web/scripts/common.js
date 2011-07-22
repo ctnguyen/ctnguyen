81bc7a0fa4564188f416248702dcd7c2c69b752d
chithanhnguyen.math <chithanhnguyen.math@gmail.com>
2011-07-22 12:52:25 +0000

    UP : already for dynamic item navigating

--- ctn_web/scripts/common.js
+++ ctn_web/scripts/common.js
@@ -11,3 +11,14 @@ function convertToPx(value_em)
 	var px_value_by_1em = parseFloat($("body").css("font-size"));
 	return value_em*px_value_by_1em;	
 }
+
+
+function get_lang()
+{
+	return 'fr';//for instant
+}
+
+function set_lang(_lang)
+{
+	//setting the user lang  : cookie or somthing in a register 
+}
