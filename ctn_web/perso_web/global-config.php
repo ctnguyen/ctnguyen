25e857d742681fad12123a6af47e63fc4e9e6581
chithanhnguyen.math <chithanhnguyen.math@gmail.com>
2012-11-14 22:29:00 +0000

    clean global config, change function name to initCookie

--- ctn_web/perso_web/global-config.php
+++ ctn_web/perso_web/global-config.php
@@ -11,23 +11,16 @@ class GlobalConfig
 	const DOMAINE_NAME      = 'localhost/';
 	const DEFAULT_LANG      = 'en';
 	
-	/** constructor for global config. setting cookies to the default value if it is not setted
-	 *  the cookie time is usually setted to the time estimated of a visitor 
+	/** initialize the lang cookie.
+	 * if  _COOKIE['lang'] is not 'en', nor 'fr', nor 'vn',
+	 * set it to de default lange i.e 'en' 
 	 */
-	public static function init($_cookie_time = 3600)
+	public static function init_cookie_lang($_cookie_time = 3600)
 	{
 		if( ($_COOKIE['lang'] !=='en') && ($_COOKIE['lang'] !=='fr') && ($_COOKIE['lang'] !=='vn') )
 		{
 			setcookie("lang", self::DEFAULT_LANG , time()+$_cookie_time);
 		}
-	}
-	
-	// Debug by printing global config constants
-	public static  function get_global_config()
-	{
-		echo '<h1>SERVER_ROOT_DIR : [' . self::SERVER_ROOT_DIR . ']</h1>';
-		echo '<h1>DOMAINE_NAME    : [' . self::DOMAINE_NAME    . ']</h1>';
-		echo '<h1>DEFAULT_LANG    : [' . self::DEFAULT_LANG    . ']</h1>';
 	}
 }
 
--- ctn_web/perso_web/test/global-config_test.php
+++ ctn_web/perso_web/test/global-config_test.php
@@ -11,8 +11,13 @@ include '../global-config.php';
 /* test if the cookie lang is setted after the constructor of GlobalConfig object
  * to set the time global small
  */
-GlobalConfig::get_global_config();
-GlobalConfig::init(5);
+
+// Debug by printing global config constants
+echo '<h1>SERVER_ROOT_DIR : [' . GlobalConfig::SERVER_ROOT_DIR . ']</h1>';
+echo '<h1>DOMAINE_NAME    : [' . GlobalConfig::DOMAINE_NAME    . ']</h1>';
+echo '<h1>DEFAULT_LANG    : [' . GlobalConfig::DEFAULT_LANG    . ']</h1>';
+
+GlobalConfig::init_cookie_lang(5);
 if( ($_COOKIE['lang'] !=='en') && ($_COOKIE['lang'] !=='fr') && ($_COOKIE['lang'] !=='vn') )
 {
 	echo '<h1>cookie lang is not setted in tree lang en,fr,vn</h1>';
