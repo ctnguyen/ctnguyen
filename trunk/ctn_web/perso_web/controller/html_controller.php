d9f03be81892e6035ba38ee86797310b163547e6
chithanhnguyen.math@gmail.com <chithanhnguyen.math@gmail.com@d822209c-5b66-c3cb-24dc-bd16cd606af5>
2012-11-14 21:35:03 +0000

    complete data in xml controller data file

--- trunk/ctn_web/perso_web/controller/html_controller.php
+++ trunk/ctn_web/perso_web/controller/html_controller.php
@@ -17,18 +17,13 @@ class HtmlController
 		$xmlDoc->load($datafilename);
 
 		$langnode   = $xmlDoc->getElementsByTagName($_lang)->item(0);
-		//$langnode       = $langnodelist->item(0);
-		$buttonnamelist = $langnode->getElementsByTagName("buttonname");
+		$buttonlist = $langnode->getElementsByTagName("htmlbutton");
 		
 		$this->htmlcontent = '';
 		$this->htmlcontent .= '<div id="navigatorlist">'.PHP_EOL;
-		$this->htmlcontent .= '<ul>'.PHP_EOL;
-		foreach($buttonnamelist as $buttonname){
-			$this->htmlcontent .= '<li>';
-			$this->htmlcontent .= $buttonname->nodeValue;
-			$this->htmlcontent .= '</li>'.PHP_EOL;
+		foreach($buttonlist as $htmlbutton){
+			$this->htmlcontent .= '	'.$htmlbutton->nodeValue.PHP_EOL;
 		}
-		$this->htmlcontent .= '</ul>'.PHP_EOL;
 		$this->htmlcontent .= '</div>'.PHP_EOL;
 	}	
 }
--- trunk/ctn_web/perso_web/test/html_controller_test.php
+++ trunk/ctn_web/perso_web/test/html_controller_test.php
@@ -3,11 +3,11 @@ include '../controller/html_controller.php';
 
 $html_en = new HtmlController('en');
 echo $html_en->htmlcontent;
-/*
+
 $html_fr = new HtmlController('fr');
 echo $html_fr->htmlcontent;
 
 $html_vn = new HtmlController('vn');
 echo $html_vn->htmlcontent;
-*/
+
 ?>
