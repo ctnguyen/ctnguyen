edd46a0501d603107e824f150f787653be3d4e78
chithanhnguyen.math <chithanhnguyen.math@gmail.com>
2012-11-15 02:29:57 +0000

    redefine xml data base file

--- ctn_web/perso_web/test/html_controller_test.php
+++ ctn_web/perso_web/test/html_controller_test.php
@@ -1,6 +1,8 @@
 <?php
 include '../controller/html_controller.php';
 
+$time_start = microtime(true);
+
 $html_en = new HtmlController('en');
 echo $html_en->htmlcontent;
 
@@ -10,4 +12,7 @@ echo $html_fr->htmlcontent;
 $html_vn = new HtmlController('vn');
 echo $html_vn->htmlcontent;
 
+$time_end = microtime(true);
+$elapsed_time = ($time_end - $time_start);
+echo '<div><b>Total Execution Time: '.$elapsed_time.' seconds</b></div>';
 ?>
