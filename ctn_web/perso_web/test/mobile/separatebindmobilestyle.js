9d9be4470b5adbc395eb6e8686d90719fa488a31
chithanhnguyen.math <chithanhnguyen.math@gmail.com>
2012-11-15 21:09:50 +0000

    add test for style on mobile

---
+++ ctn_web/perso_web/test/mobile/separatebindmobilestyle.js
@@ -0,0 +1,8 @@
+
+function bindmobilestyle()
+{
+$('#page').attr('data-role','page');
+$('#header').attr('data-role','header');
+$('#content').attr('data-role','content');
+$('#footer').attr('data-role','footer');
+}
