9ac222c7a5b095b3a04b9985294bf0671873a636
chithanhnguyen.math@gmail.com <chithanhnguyen.math@gmail.com@d822209c-5b66-c3cb-24dc-bd16cd606af5>
2012-11-16 23:07:11 +0000

    global layout works

--- trunk/ctn_web/perso_web/view/style/global_normalbrowser_layout.js
+++ trunk/ctn_web/perso_web/view/style/global_normalbrowser_layout.js
@@ -31,23 +31,45 @@ function set_global_layout()
 		,"margin-left":"10em"
 //			,"margin-right":"auto"
 	});
+	
+	$('#top-page').css({
+		"margin":"0 0 0 0"
+		,"padding":"0 0 0 0"
+		,"clear":"both"
+	});
+	
 	$('#navigator').css({
 		"width":_get_sidebar_width()
 		,"margin-right":"0"
+			,"float":"left"
 //			,"border-right-style":"groove"
 //			,"border-right-width":"5px"
 //			,"border-right-color":"red"
 	});
 	
+	$('#logo').css({
+		"display":"block"
+		,"width":"80%"
+	    ,"margin-left":"auto"
+	    ,"margin-right":"auto"
+	});
+	
 	$('#maincontent').css({
 		"width":_get_content_width()
 		,"margin-right":"0"
-			,"padding-left":"2em"
+			,"float":"right"
 				,"border-left-style":"groove"
 					,"border-left-width":"1px"
 						,"border-left-color":"#000000"
 	});
 
+
+	$('#botton-page').css({
+		"margin":"0 0 0 0"
+		,"padding":"0 0 0 0"
+		,"clear":"both"
+	});
+	
 	$('#footer').css({
 		"height":"100px"
 			,"font-size":"1em" 
@@ -59,5 +81,20 @@ function set_global_layout()
 			,"padding-top":"40px"
 	});
 	*/
-//	_debug_layout();
+	debug_layout();
+}
+
+function debug_layout()
+{
+	//plot border of all bloc
+	//$('body').css({"border":"1px solid #ffffff"});
+	$('#whole-page').css({"border":"1px solid #00FF00"});
+	//$('#page-header').css({"border":"1px solid #D2691E"});
+	$('#top-page').css({"border":"1px solid #808080"});
+	//$('#page').css({"border":"1px solid #008000"});
+	$('#maincontent').css({"border":"1px solid #00BFFF"});
+	$('#navigator').css({"border":"1px solid #0000FF"});
+	$('#botton-page').css({"border":"1px solid #808080"});
+	$('#footer').css({"border":"1px solid #D2691E"});
+	//$('ul._sidebar.menu').css({"border":"1px solid black"});
 }
