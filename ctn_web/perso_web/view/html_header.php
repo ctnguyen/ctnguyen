f33e75687207000876600c845240b946c00936c4
chithanhnguyen.math <chithanhnguyen.math@gmail.com>
2012-11-16 16:42:14 +0000

    add test style for specific content

--- ctn_web/perso_web/view/html_header.php
+++ ctn_web/perso_web/view/html_header.php
@@ -29,14 +29,14 @@ class HtmlHeader
 			$this->htmlcontent .= '	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.css" />'.PHP_EOL;
 			$this->htmlcontent .= '	<script src="http://code.jquery.com/jquery-1.8.2.min.js"></script>'.PHP_EOL;
 			$this->htmlcontent .= '	<script src="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.js"></script>'.PHP_EOL;
-		
-			$auxStyleBindRelativePath .= 'view/mobi_'.$_general_request->_content_state . '.js';
+			
+			$this->htmlcontent .= '	<script src="'.GlobalConfig::DOMAINE_NAME .'view/style/mobibrowser_globallayout.js"></script>'.PHP_EOL;
+			$auxStyleBindRelativePath .= 'view/style/mobi_'.$_general_request->_content_state . '.js';
 		}
 		else
 		{
-			$this->htmlcontent .= '	<script src="'.GlobalConfig::DOMAINE_NAME .'view/normalbrowser_globallayout.js"></script>'.PHP_EOL;
-		
-			$auxStyleBindRelativePath .= 'view/'.$_general_request->_content_state . '.js';
+			$this->htmlcontent .= '	<script src="'.GlobalConfig::DOMAINE_NAME .'view/style/normalbrowser_globallayout.js"></script>'.PHP_EOL;
+			$auxStyleBindRelativePath .= 'view/style/'.$_general_request->_content_state .'.js';
 		}
 		
 		$this->htmlcontent .= '<!--'.PHP_EOL;
@@ -44,7 +44,7 @@ class HtmlHeader
 		$this->htmlcontent .= ' Each time user change content, this part will also be updated'.PHP_EOL;
 		$this->htmlcontent .= ' The file name rule is view/[<mobile?>_][contentID].js'.PHP_EOL;
 		$this->htmlcontent .= '-->'.PHP_EOL;
-		$this->htmlcontent .= '	<script class="auxiliarstyle" src="'. GlobalConfig::DOMAINE_NAME . $auxStyleBindRelativePath .' "></script>'.PHP_EOL;
+		$this->htmlcontent .= '	<script class="auxiliarstyle" src="'. GlobalConfig::DOMAINE_NAME . $auxStyleBindRelativePath .'"></script>'.PHP_EOL;
 	}
 }
 
