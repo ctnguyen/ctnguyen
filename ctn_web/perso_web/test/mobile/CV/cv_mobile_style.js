6231a78b188df223eda25cb851420d18dd0864db
chithanhnguyen.math <chithanhnguyen.math@gmail.com>
2012-11-16 13:11:43 +0000

    add tests for mobile

---
+++ ctn_web/perso_web/test/mobile/CV/cv_mobile_style.js
@@ -0,0 +1,8 @@
+function bindmobilecvstyle() 
+{
+	$('#content').attr('data-role', 'page');
+	$('#title').attr('data-role', 'header');
+	$('h2').attr('data-role', 'header');
+	// $('#education').attr('data-role','content');
+	// $('#footer').attr('data-role','footer');
+}
