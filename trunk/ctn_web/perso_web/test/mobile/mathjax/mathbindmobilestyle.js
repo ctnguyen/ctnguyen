4ad68c8973577a4929a3eb00746437d73a2ab956
chithanhnguyen.math@gmail.com <chithanhnguyen.math@gmail.com@d822209c-5b66-c3cb-24dc-bd16cd606af5>
2012-11-16 13:11:43 +0000

    add tests for mobile

---
+++ trunk/ctn_web/perso_web/test/mobile/mathjax/mathbindmobilestyle.js
@@ -0,0 +1,8 @@
+
+function mathbindmobilestyle()
+{
+$('#page').attr('data-role','page');
+$('#header').attr('data-role','header');
+$('#content').attr('data-role','content');
+$('#footer').attr('data-role','footer');
+}
