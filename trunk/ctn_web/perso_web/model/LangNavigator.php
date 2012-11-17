5796ef473fafdfae1aff193759ab3db06f72e0bc
chithanhnguyen.math@gmail.com <chithanhnguyen.math@gmail.com@d822209c-5b66-c3cb-24dc-bd16cd606af5>
2012-11-17 02:31:58 +0000

    complete works for global layout

--- trunk/ctn_web/perso_web/model/LangNavigator.php
+++ trunk/ctn_web/perso_web/model/LangNavigator.php
@@ -8,21 +8,21 @@ class LangNavigator
 	public function __construct()
 	{
 		$this->htmlcontent='';
-		$this->htmlcontent .= '		<div id="langnavigator">'.PHP_EOL;
-		$this->htmlcontent .= '			<table>'.PHP_EOL;
-		$this->htmlcontent .= '				<tr>'.PHP_EOL;
+		$this->htmlcontent .= '				<div id="langnavigator">'.PHP_EOL;
+		$this->htmlcontent .= '					<table>'.PHP_EOL;
+		$this->htmlcontent .= '						<tr>'.PHP_EOL;
 
-		$this->htmlcontent .= '					<td><img id="en" class="langbutton" src="'. GlobalConfig::DOMAINE_NAME . 'model/htmldata/image/icon_en.png"/></td>'.PHP_EOL;
-		$this->htmlcontent .= '					<td><img id="fr" class="langbutton" src="'. GlobalConfig::DOMAINE_NAME . 'model/htmldata/image/icon_fr.png"/></td>'.PHP_EOL;
-		$this->htmlcontent .= '					<td><img id="vn" class="langbutton" src="'. GlobalConfig::DOMAINE_NAME . 'model/htmldata/image/icon_vn.png"/></td>'.PHP_EOL;
+		$this->htmlcontent .= '							<td><img id="en" class="langbutton" src="'. GlobalConfig::DOMAINE_NAME . 'model/htmldata/image/icon_en.png"/></td>'.PHP_EOL;
+		$this->htmlcontent .= '							<td><img id="fr" class="langbutton" src="'. GlobalConfig::DOMAINE_NAME . 'model/htmldata/image/icon_fr.png"/></td>'.PHP_EOL;
+		$this->htmlcontent .= '							<td><img id="vn" class="langbutton" src="'. GlobalConfig::DOMAINE_NAME . 'model/htmldata/image/icon_vn.png"/></td>'.PHP_EOL;
 		
-		$this->htmlcontent .= '				</tr>'.PHP_EOL;
-		$this->htmlcontent .= '			</table>'.PHP_EOL;
-		$this->htmlcontent .= '		</div>'.PHP_EOL;
+		$this->htmlcontent .= '						</tr>'.PHP_EOL;
+		$this->htmlcontent .= '					</table>'.PHP_EOL;
+		$this->htmlcontent .= '				</div>'.PHP_EOL;
 		
-		$this->htmlcontent .= '		<div align="center">'.PHP_EOL;
-		$this->htmlcontent .= '				<img id="logo" src="'. GlobalConfig::DOMAINE_NAME . 'model/htmldata/image/ctn.jpg"/>'.PHP_EOL;
-		$this->htmlcontent .= '		</div>'.PHP_EOL;
+		$this->htmlcontent .= '				<div align="center">'.PHP_EOL;
+		$this->htmlcontent .= '					<img id="logo" src="'. GlobalConfig::DOMAINE_NAME . 'model/htmldata/image/ctn.jpg"/>'.PHP_EOL;
+		$this->htmlcontent .= '				</div>'.PHP_EOL;
 	}
 }
 
