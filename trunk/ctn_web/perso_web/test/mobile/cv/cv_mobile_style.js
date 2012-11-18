5e2799f10c4b31404ffc9b5764db4217c4462ba2
chithanhnguyen.math@gmail.com <chithanhnguyen.math@gmail.com@d822209c-5b66-c3cb-24dc-bd16cd606af5>
2012-11-18 18:56:55 +0000

    experiences style for CV mobile

--- trunk/ctn_web/perso_web/test/mobile/cv/cv_mobile_style.js
+++ trunk/ctn_web/perso_web/test/mobile/cv/cv_mobile_style.js
@@ -1,9 +1,27 @@
 function bindmobilecvstyle() 
 {
+	//mobile global layout 
 	$('#whole-page').attr('data-role', 'page');
 	$('#navigator').attr('data-role', 'header');
 	$('#contentnavigator').attr('data-role', 'navbar');
 	$('#content').attr('data-role','content');
-	$('h2').attr('class', 'ui-bar ui-bar-a');
 	$('#page-footer').attr('data-role','footer');
+	
+	
+	// CV mobile style 
+	$('.xpdate').css({"float":"left"});
+	$('.xpenterprise').css({"float":"right"});
+	
+	$('.xptechnolabel').css({"float":"left","margin-right":"3em"});
+	$('.xptechnolist').css({"float":"left"});
+
+	$('.clearfloatting').css({"clear":"both"});
+
+	
+	$('h2').attr('data-role','header')          ; $('h2').attr('data-theme','b');
+	$('.xpdatebar').attr('data-role','header')  ; $('.xpdatebar').attr('data-theme','c');
+	$('.xptechnobar').attr('data-role','footer'); $('.xptechnobar').attr('data-theme','c');
+	
+	$('.xpItem').attr('data-role','content'); $('.xpItem').attr('data-theme','c');
+	
 }
