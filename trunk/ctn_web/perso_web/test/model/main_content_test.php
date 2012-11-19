3f26f44d63be26d58ff951a9eb751a849f924647
chithanhnguyen.math@gmail.com <chithanhnguyen.math@gmail.com@d822209c-5b66-c3cb-24dc-bd16cd606af5>
2012-11-19 22:26:46 +0000

    add indents as parameters for building MainContent container

--- trunk/ctn_web/perso_web/test/model/main_content_test.php
+++ trunk/ctn_web/perso_web/test/model/main_content_test.php
@@ -1,7 +1,7 @@
 <?php 
 	require_once realpath( dirname(__FILE__ ) . '/../../global-config.php');
 	require_once GlobalConfig::SERVER_ROOT_DIR.'controller/GeneralRequestState.php';
-	require_once GlobalConfig::SERVER_ROOT_DIR.'model/MMainContent.php';
+	require_once GlobalConfig::SERVER_ROOT_DIR.'model/MainContent.php';
 ?>
 
 <html>
