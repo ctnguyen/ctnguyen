e8b3c98ff32e4ce05c89cb2d1017524915596311
chithanhnguyen.math <chithanhnguyen.math@gmail.com>
2012-11-19 22:26:46 +0000

    add indents as parameters for building MainContent container

--- ctn_web/perso_web/model/MainContent.php
+++ ctn_web/perso_web/model/MainContent.php
@@ -13,18 +13,33 @@ class MainContent
 {
 	public $htmlcontent;
 	
-	public function __construct($_general_request)
+	public function __construct($_general_request,$nbIndent=0)
 	{
 		//get path to the html file.
 		$filepath  = GlobalConfig::SERVER_ROOT_DIR.'model/htmldata/';
 		$filepath .= $_general_request->_lang_state . '/';
 		$filepath .= $_general_request->_content_state . '.html';
 		
+		$additionalInden = '';
+		for($i=0; $i< $nbIndent ; $i++)
+		{
+			$additionalInden .= '	';
+		}
+		
+		
+		$htmlcontent = '';
 		if( file_exists($filepath) )
 		{
-			$handle = fopen($filepath, 'r');
-			$this->htmlcontent = fread($handle, filesize($filepath));
-			fclose($handle);
+			$handle = @fopen($filepath, 'r');
+			if ($handle) {
+				while (($buffer = fgets($handle, 4096)) !== false) {
+					$this->htmlcontent .= $additionalInden . $buffer;
+				}
+				if (!feof($handle)) {
+					$this->htmlcontent .= '<p style="color:red"><b>Error: unexpected fgets() fail</b></p>'.PHP_EOL;
+				}
+				fclose($handle);
+			}	
 		}
 		else
 		{
