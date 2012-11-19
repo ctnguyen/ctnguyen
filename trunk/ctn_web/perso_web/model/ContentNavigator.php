21d401d2a571eff95ebe5aa7c8ea26ddf84ca023
chithanhnguyen.math@gmail.com <chithanhnguyen.math@gmail.com@d822209c-5b66-c3cb-24dc-bd16cd606af5>
2012-11-19 23:05:49 +0000

    add indent and change argument constructor to GeneralRequestState for ContentNavigator and LangNavigator

--- trunk/ctn_web/perso_web/model/ContentNavigator.php
+++ trunk/ctn_web/perso_web/model/ContentNavigator.php
@@ -1,5 +1,6 @@
 <?php
 require_once realpath( dirname(__FILE__ ) . '/../global-config.php');
+require_once GlobalConfig::SERVER_ROOT_DIR.'controller/GeneralRequestState.php';
 
 /* class returning the content html of all content
  * concretly this is the sidebar in the web
@@ -8,40 +9,45 @@ class ContentNavigator
 {
 	public $htmlcontent;
 	
-	public function __construct($_lang)
+	public function __construct($_general_request , $nbIndent=0)
 	{
+		$additionalInden = '';
+		for($i=0; $i< $nbIndent ; $i++)
+		{
+			$additionalInden .= '	';
+		}
+		
 		$datafilename = '';
 		$datafilename .= GlobalConfig::SERVER_ROOT_DIR.'controller/data/NavigatorList.xml';
 		
 		$xmlDoc = new DOMDocument;
 		$xmlDoc->load($datafilename);
 
-		$langnode   = $xmlDoc->getElementsByTagName($_lang)->item(0);
+		$langnode   = $xmlDoc->getElementsByTagName($_general_request->_lang_state)->item(0);
 		$buttonlist = $langnode->getElementsByTagName("navigatebutton");
 		
 		$this->htmlcontent = '';
-		$this->htmlcontent .= '				<div id="contentnavigator">'.PHP_EOL;
-		$this->htmlcontent .= '					<ul>'.PHP_EOL;
+		$this->htmlcontent .= $additionalInden.'<div id="contentnavigator">'.PHP_EOL;
+		$this->htmlcontent .= $additionalInden.'	<ul>'.PHP_EOL;
 		
 		foreach($buttonlist as $button)
 		{
 			$buttonID = $button->getElementsByTagName("idNAME")->item(0);
 			if($buttonID)
 			{
-				$this->htmlcontent .= '						<li><a ';
+				$this->htmlcontent .= $additionalInden.'		<li><a ';
 				$this->htmlcontent .= 'id="'.$buttonID->nodeValue.'" class="navigatebutton"';
 				$this->htmlcontent .= '>'.$button->getElementsByTagName("htmlNAME")->item(0)->nodeValue;
 				$this->htmlcontent .= '</a></li>'.PHP_EOL;
 			}
 			else
 			{
-				$this->htmlcontent .= '						'.$button->getElementsByTagName("htmlNAME")->item(0)->nodeValue.PHP_EOL;;
+				$this->htmlcontent .= $additionalInden.'		'.$button->getElementsByTagName("htmlNAME")->item(0)->nodeValue.PHP_EOL;;
 			}
-			
 		}
 		
-		$this->htmlcontent .= '					</ul>'.PHP_EOL;
-		$this->htmlcontent .= '				</div>'.PHP_EOL;
+		$this->htmlcontent .= $additionalInden.'		</ul>'.PHP_EOL;
+		$this->htmlcontent .= $additionalInden.'</div>'.PHP_EOL;
 	}	
 }
 
--- trunk/ctn_web/perso_web/model/LangNavigator.php
+++ trunk/ctn_web/perso_web/model/LangNavigator.php
@@ -1,28 +1,35 @@
 <?php
 require_once realpath( dirname(__FILE__ ) . '/../global-config.php');
+require_once GlobalConfig::SERVER_ROOT_DIR.'controller/GeneralRequestState.php';
 
 class LangNavigator
 {
 	public $htmlcontent;
 	
-	public function __construct()
+	public function __construct($_general_request , $nbIndent=0)
 	{
+		$indent = '';
+		for($i=0; $i< $nbIndent ; $i++)
+		{
+			$indent .= '	';
+		}
+		
 		$this->htmlcontent='';
-		$this->htmlcontent .= '				<div id="langnavigator">'.PHP_EOL;
-		$this->htmlcontent .= '					<table>'.PHP_EOL;
-		$this->htmlcontent .= '						<tr>'.PHP_EOL;
+		$this->htmlcontent .= $indent.'<div id="langnavigator">'.PHP_EOL;
+		$this->htmlcontent .= $indent.'	<table>'.PHP_EOL;
+		$this->htmlcontent .= $indent.'		<tr>'.PHP_EOL;
 
-		$this->htmlcontent .= '							<td><img id="en" class="langbutton" src="'. GlobalConfig::DOMAINE_NAME . 'model/htmldata/image/icon_en.png"/></td>'.PHP_EOL;
-		$this->htmlcontent .= '							<td><img id="fr" class="langbutton" src="'. GlobalConfig::DOMAINE_NAME . 'model/htmldata/image/icon_fr.png"/></td>'.PHP_EOL;
-		$this->htmlcontent .= '							<td><img id="vn" class="langbutton" src="'. GlobalConfig::DOMAINE_NAME . 'model/htmldata/image/icon_vn.png"/></td>'.PHP_EOL;
+		$this->htmlcontent .= $indent.'			<td><img id="en" class="langbutton" src="'. GlobalConfig::DOMAINE_NAME . 'model/htmldata/image/icon_en.png"/></td>'.PHP_EOL;
+		$this->htmlcontent .= $indent.'			<td><img id="fr" class="langbutton" src="'. GlobalConfig::DOMAINE_NAME . 'model/htmldata/image/icon_fr.png"/></td>'.PHP_EOL;
+		$this->htmlcontent .= $indent.'			<td><img id="vn" class="langbutton" src="'. GlobalConfig::DOMAINE_NAME . 'model/htmldata/image/icon_vn.png"/></td>'.PHP_EOL;
 		
-		$this->htmlcontent .= '						</tr>'.PHP_EOL;
-		$this->htmlcontent .= '					</table>'.PHP_EOL;
-		$this->htmlcontent .= '				</div>'.PHP_EOL;
+		$this->htmlcontent .= $indent.'		</tr>'.PHP_EOL;
+		$this->htmlcontent .= $indent.'	</table>'.PHP_EOL;
+		$this->htmlcontent .= $indent.'</div>'.PHP_EOL;
 		
-		$this->htmlcontent .= '				<div align="center">'.PHP_EOL;
-		$this->htmlcontent .= '					<img id="logo" src="'. GlobalConfig::DOMAINE_NAME . 'model/htmldata/image/ctn.jpg"/>'.PHP_EOL;
-		$this->htmlcontent .= '				</div>'.PHP_EOL;
+		$this->htmlcontent .= $indent.'<div align="center">'.PHP_EOL;
+		$this->htmlcontent .= $indent.'<img id="logo" src="'. GlobalConfig::DOMAINE_NAME . 'model/htmldata/image/ctn.jpg"/>'.PHP_EOL;
+		$this->htmlcontent .= $indent.'</div>'.PHP_EOL;
 	}
 }
 
