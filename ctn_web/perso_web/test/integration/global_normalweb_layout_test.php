c8a40ab4f38c0ea293ded070495892d0b1b153a1
chithanhnguyen.math <chithanhnguyen.math@gmail.com>
2012-11-19 00:02:03 +0000

    add referencing div

--- ctn_web/perso_web/test/integration/global_normalweb_layout_test.php
+++ ctn_web/perso_web/test/integration/global_normalweb_layout_test.php
@@ -46,7 +46,11 @@ $headercontent = ModelHelper::getHtmlHeader();
 echo $headercontent;
 
 echo '		<div id="whole-page">'.PHP_EOL;
-echo '			<div id="top-page">&nbsp;&nbsp;</div>'.PHP_EOL;
+echo '			<div id="top-page">'.PHP_EOL;
+echo '				<div class="referencing"><h1>NGUYEN Chi Thanh, INRIA, SOFA, Digiplante, Paris VI, Paris 6</h1></div>'.PHP_EOL;
+echo '				<div id="actuallangue" style="display:none;">fr</div>'.PHP_EOL;
+echo '				<div id="actuallnavication" style="display:none;">home</div>'.PHP_EOL;
+echo '			</div>'.PHP_EOL;
 
 $htmlNavigator = ModelHelper::getHtmlNavigator($rstate);
 echo $htmlNavigator;
@@ -54,12 +58,31 @@ echo $htmlNavigator;
 $maincontent = ModelHelper::getHtmlMainContent($rstate);
 echo $maincontent;
 
-echo '			<div id="botton-page">&nbsp;&nbsp;</div>'.PHP_EOL;
+echo '			<div id="bottom-page">'.PHP_EOL;
+echo '				<div class="referencing"><h1>NGUYEN Chi Thanh, INRIA, SOFA, Digiplante, Paris VI, Paris 6</h1></div>'.PHP_EOL;
+echo '			</div>'.PHP_EOL;
+
 
 echo '		</div>'.PHP_EOL;
 $footercontent = ModelHelper::getHtmlFooter($rstate->_lang_state);
 echo $footercontent;
 echo '	</div>'.PHP_EOL;
+
+echo '	<script type="text/javascript">'.PHP_EOL          ;
+//echo '		$(".referencing").remove();'.PHP_EOL.PHP_EOL          ;
+
+echo '		$("img.langbutton").click(function() {'.PHP_EOL     ;
+echo '			reset_actuallang($(this).attr("id"));'.PHP_EOL  ;
+echo '			reload_allpage($(this).attr("id"));'.PHP_EOL ;
+echo '		});'.PHP_EOL                                  ;
+
+echo '		$("a.navigatebutton").click(function() {'.PHP_EOL     ;
+echo '			reset_actualnaviation(attr("id"));'.PHP_EOL  ;
+echo '			reload_maincontent($(this).attr("id"));'.PHP_EOL ;
+echo '		});'.PHP_EOL                                  ;
+
+echo '	</script>'.PHP_EOL                                ;
+
 ?>
 </body>
 </html>
