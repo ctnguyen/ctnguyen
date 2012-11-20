94c5d34a08006c73b6c8dbf0c9608b943670b094
chithanhnguyen.math <chithanhnguyen.math@gmail.com>
2012-11-20 21:14:36 +0000

    add globale layout style for mobile

---
+++ ctn_web/perso_web/view/style/mobibrowser_G_setting.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+	set_global_layout();
+});
+
+/*
+	page-wrapper     [non for mobile]
+	 top-wrapper     [non for mobile]
+  	whole-page
+	 top-page        
+sidebar     content
+	 bottom-page      
+	bottom-wrapper   [different for mobile (included in the whole page instead of a wrapper)]
+ */
+
+function set_global_layout()
+{
+	// mobile globale layout ================================================ 
+	$('#whole-page').attr('data-role', 'page');
+	$('#navigator').attr('data-role', 'header');
+	$('#langnavigator').attr('data-role', 'navbar');
+	$('#contentnavigator').attr('data-role', 'navbar');
+	$('#content').attr('data-role','content');
+	$('#bottom-wrapper').attr('data-role','footer');
+}
