09450b3b3b008b386091b0a6b527afadd162c4b0
chithanhnguyen.math <chithanhnguyen.math@gmail.com>
2012-11-21 21:10:31 +0000

    a little change of initialize for mobile browser

--- ctn_web/perso_web/test/mobile/bindinglate/separatebindmobilestyle.js
+++ ctn_web/perso_web/test/mobile/bindinglate/separatebindmobilestyle.js
@@ -1,8 +1,13 @@
 
+function initialize_mobile_browser()
+{
+	bindmobilestyle();
+}
+
 function bindmobilestyle()
 {
-$('#page').attr('data-role','page');
-$('#header').attr('data-role','header');
-$('#content').attr('data-role','content');
-$('#footer').attr('data-role','footer');
+	$('#page').attr('data-role','page');
+	$('#header').attr('data-role','header');
+	$('#content').attr('data-role','content');
+	$('#footer').attr('data-role','footer');
 }
