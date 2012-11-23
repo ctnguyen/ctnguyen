27cb31cfb1d43be9e719c5e8497807d749414606
chithanhnguyen.math@gmail.com <chithanhnguyen.math@gmail.com@d822209c-5b66-c3cb-24dc-bd16cd606af5>
2012-11-23 00:45:38 +0000

    update for ajax call, data receiving for instant is text data, need to use xml data in order to write to html DOM

--- trunk/ctn_web/perso_web/view/style/normalbrowser_G_setting.js
+++ trunk/ctn_web/perso_web/view/style/normalbrowser_G_setting.js
@@ -32,7 +32,7 @@ sidebar     content
 	bottom-wrapper   [different for mobile (included in the whole page instead of a wrapper)]
  */
 
-// page width depend on how many character it has, so its convenient to set it in relation to em
+//page width depend on how many character it has, so its convenient to set it in relation to em
 function _get_page_width(){return '80em';}
 function _get_sidebar_width(){return '22%';}
 function _get_content_width(){return '70%';}
@@ -44,81 +44,81 @@ function set_global_normal_layout()
 {
 	$('body').css({
 		"margin":"0 0 0 0"
-		,"padding":"0 0 0 0"
+			,"padding":"0 0 0 0"
 	});
-	
+
 	$('#page-wrapper').css({
 		"margin":"0 0 0 0"
-		,"padding":"0 0 0 0"
+			,"padding":"0 0 0 0"
 	});
 
 	$('#top-wrapper').css({
 		"height":"100px"
-		,"width":$(window).width()
-		,"margin":"0 0 0 0"
-		,"padding":"0 0 0 0"
+			,"width":$(window).width()
+			,"margin":"0 0 0 0"
+				,"padding":"0 0 0 0"
 	});
-	
+
 	$('#whole-page').css({
 		"width":_get_page_width()
 		,"margin-left":"10em"
 //			,"margin-right":"auto"
 	});
-	
+
 	$('#top-page').css({
 		"margin":"0 0 0 0"
-		,"padding":"0 0 0 0"
-		,"clear":"both"
+			,"padding":"0 0 0 0"
+				,"clear":"both"
 	});
-	
+
 	$('#navigator').css({
 		"width":_get_sidebar_width()
 		,"margin-right":"0"
 			,"float":"left"
-//			,"border-right-style":"groove"
-//			,"border-right-width":"5px"
-//			,"border-right-color":"red"
+//				,"border-right-style":"groove"
+//				,"border-right-width":"5px"
+//				,"border-right-color":"red"
 	});
-	
+
 	$('#logo').css({
 		"display":"block"
-		,"width":"80%"
-	    ,"margin-left":"auto"
-	    ,"margin-right":"auto"
+			,"width":"80%"
+				,"margin-left":"auto"
+					,"margin-right":"auto"
 	});
-	
+
 	$('#maincontent').css({
 		"width":_get_content_width()
 		,"margin-right":"0"
-		,"float":"right"
-		,"border-left-style":"groove"
-		,"border-left-width":"1px"
-		,"border-left-color":"#000000"
+			,"float":"right"
+				,"border-left-style":"groove"
+					,"border-left-width":"1px"
+						,"border-left-color":"#000000"
 	});
 
 
 	$('#bottom-page').css({
 		"margin":"0 0 0 0"
-		,"padding":"0 0 0 0"
-		,"clear":"both"
+			,"padding":"0 0 0 0"
+				,"clear":"both"
 	});
-	
+
 	$('#bottom-wrapper').css({
 		"height":"100px"
-		,"width":$(window).width()
-		,"font-size":"1em"
-		,"margin":"0 0 0 0"
-		,"padding":"0 0 0 0"
-		,"text-align":"center"
-		,"position":"fixed"
-		,"bottom":"0px"
+			,"width":$(window).width()
+			,"font-size":"1em"
+				,"margin":"0 0 0 0"
+					,"padding":"0 0 0 0"
+						,"text-align":"center"
+							,"position":"fixed"
+								,"bottom":"0px"
 	});
 
 	$('#page-footer').css({		
 		"font-size":"1em" 
-		,"padding-top":"40px"
+			,"padding-top":"40px"
 	});
-	
+
 	debug_layout();
 }
 
@@ -138,7 +138,7 @@ function debug_layout()
 function update_langue(newlang)
 {
 	alert('actual lang is '+ get_actual_lang_state() ); //Debug
-	
+
 	reset_actual_lang_state(newlang);
 	alert('new lang is '+ get_actual_lang_state() );    //Debug
 }
@@ -146,30 +146,26 @@ function update_langue(newlang)
 function update_maincontent(newcontentlabel)
 {
 	alert('actual content is '+ get_actual_content_state() );    //Debug
-	
+
 	var lang_request    = get_actual_lang_state();
 	var content_request = newcontentlabel;
 	var isMobile_request= 'false';
+
+	$.ajax({
+		type: 'GET'
+		,url : 'controller/UpdateContent.php'
+		,data:{'lang':lang_request,'content':content_request,'isMobile':isMobile_request}
+		//,dataType: 'xml'
+	}).done(function( msg ) {
+		alert( 'ajax done mgs [' + msg + ']');
+		
+		//Replacing the div#sidebar of index page  by the one newly loaded in the uriSideBar page
+		//$('#maincontent').empty().html( $(loadedXHTML).contents().find('#maincontent').html() );
+
+	});
 	
-    $.ajax({
-    	url : 'http://localhost/controller/UpdateContent.php',
-        type: 'GET',
-        data:{'lang':lang_request,'content':content_request,'isMobile':isMobile_request},
-        cache: false,    // or set to false
-        dataType: "xml",
-        async: false,
-        success: function(loadedXHTML) {
-        	alert('ajax call success');
-    		//Replacing the div#sidebar of index page  by the one newly loaded in the uriSideBar page
-			$('#maincontent').empty().html( $(loadedXHTML).contents().find('#maincontent').html() );
-			
-			//Replacing the local style of index page by the one newly loaded in the uriSideBar page
-			//$('#contentstyle').attr('href', $(loadedXHTML).contents().find('link#contentstyle').attr('href'));
-        }
-    });
-	
-	
-	
+
+
 	reset_actual_content_state(newcontentlabel);	
 	alert('new content label is '+ get_actual_content_state() ); //Debug
 }
