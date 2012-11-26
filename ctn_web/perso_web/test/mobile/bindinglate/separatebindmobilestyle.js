1ad60b52f072daa57427b4d262eaf10b34c7ad26
chithanhnguyen.math <chithanhnguyen.math@gmail.com>
2012-11-26 10:23:34 +0000

    update bindinglate, no need to call the initialize into the body html, just defining a data-role at the begining, and then bind the 'pagebeforecreate' event of document

--- ctn_web/perso_web/test/mobile/bindinglate/separatebindmobilestyle.js
+++ ctn_web/perso_web/test/mobile/bindinglate/separatebindmobilestyle.js
@@ -1,12 +1,31 @@
+$(document).bind("mobileinit", function(){
+  alert('mobileinit fire off');
+});
+
+$( document ).bind("pagebeforecreate", function(event) {
+	alert('pagebeforecreate fire off');
+	initialize_mobile_browser();
+});
+
+
+$( document ).bind("pagecreate", function() {
+	alert('pagecreate fire off');
+});
+
+
+$( document ).bind("pageinit", function() {
+	alert('pageinit fire off');
+});
 
 function initialize_mobile_browser()
 {
 	bindmobilestyle();
+	alert('call of manually binding style');
 }
 
 function bindmobilestyle()
 {
-	$('#page').attr('data-role','page');
+	//$('#whole-page').attr('data-role','page');
 	$('#header').attr('data-role','header');
 	$('#content').attr('data-role','content');
 	$('#footer').attr('data-role','footer');
