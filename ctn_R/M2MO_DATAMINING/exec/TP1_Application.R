7d904ec593443c834f3e8804423939f28a5a26c1
chithanhnguyen.math <chithanhnguyen.math@gmail.com>
2013-09-25 19:27:43 +0000

    finish first datamining TD1_application

--- ctn_R/M2MO_DATAMINING/exec/TP1_Application.R
+++ ctn_R/M2MO_DATAMINING/exec/TP1_Application.R
@@ -1,1 +1,20 @@
-## m2mo Laure Elie TP1 Applications
+######################################################
+#######  m2mo Laure Elie TP1 Applications   ##########
+######################################################
+
+#!!! Set the PROJECT_ROOT_DIRECTORY as the working directory
+## i.e ".." from this file
+
+tab=read.table("data/immo.txt", header=TRUE, sep=";") # Read the data file from the data folder
+tab[,1];
+tab$surface;
+tab[,c(1,3)];
+tab$transaction;
+## Reponse 1 : the data containing in the immo.txt file composed of 3 variables, 20 observations
+
+plot(tab);
+## Reponse 2 : Because there are 3 variables, the plot function of the data frame plot
+##             each variable in function of other, then there are 6 graphics.
+
+cor(tab);
+## Reponse  : the variable "valeur" is hightly correlated to the variable "surface"
