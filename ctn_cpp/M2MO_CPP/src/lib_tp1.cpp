392e5014167fbeb3b1feaf4ecc6b632b6cbe1549
chithanhnguyen.math <chithanhnguyen.math@gmail.com>
2013-10-02 16:23:47 +0000

    m2mo cpp finished exercice 2

--- ctn_cpp/M2MO_CPP/src/lib_tp1.cpp
+++ ctn_cpp/M2MO_CPP/src/lib_tp1.cpp
@@ -1,10 +1,22 @@
-#include <header_tp1.hpp>
 #include <iostream>
+#include <header_tp1.hpp>
 
 
 namespace TP1
 {
 
+// look at http://en.wikipedia.org/wiki/Euclidean_algorithm
+// procedure and implementation (recursion version)
+unsigned int pgcd_r(unsigned int a, unsigned int b)
+{
+	if(a == 0) return b;
+	if(b == 0) return a;
+
+	if (a < b)
+		return pgcd_r(a,b%a);
+	else
+		return pgcd_r(b,a%b);
+}
 
 
 
