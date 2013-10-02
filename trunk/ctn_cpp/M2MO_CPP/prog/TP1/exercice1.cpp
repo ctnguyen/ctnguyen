0f4fbc96a74d4ba9364c0bc252bf3b34460859ed
chithanhnguyen.math@gmail.com <chithanhnguyen.math@gmail.com@d822209c-5b66-c3cb-24dc-bd16cd606af5>
2013-10-02 15:15:09 +0000

    m2mo cpp finished exercice 1

--- trunk/ctn_cpp/M2MO_CPP/prog/TP1/exercice1.cpp
+++ trunk/ctn_cpp/M2MO_CPP/prog/TP1/exercice1.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
-using namespace std;
+#include <cstdlib>
 
+using namespace std;
 
 
 int main(int argc,char** argv)
@@ -9,14 +10,30 @@ int main(int argc,char** argv)
 	int i;
 
 	//cout<<"****Premier programme****"<<endl;
-	cout<<"****Division****"<<endl; // Question 1
-	cout<<"Saisissez un nombre puis appuyez sur entree : ";
-
-	cin >> d;
+	cout<<std::endl<< "****Division****" <<endl; //m2mo_tp1_question_2
+	//cout<<"Saisissez un nombre puis appuyez sur entree : ";	cin >> d;
 
 	i=0; // on met 0 dans i
-	while(i<=15)
+
+	int max_N = 15;
+	i=1 ; max_N = 100; //m2mo_tp1_question_3
+	//cout<<"Saisissez un nombre indiquant le max : "; cin >> max_N; //m2mo_tp1_question_4
+
+
+	//m2mo_tp1_question_6
+	if (argc != 3) {
+		cout << "Two argument expected : excercice1 [divisor] [Max_dividend]"<<std::endl;
+		return 0;
+	}
+	else
 	{
+		d     = atoi(argv[1]) ;
+		max_N = atoi(argv[2]) ;
+	}
+
+	while(i<=max_N)
+	{
+		/*
 		if( i % d == 0)
 		{
 			cout << i << " est divisible par "<< d << endl;
@@ -26,5 +43,14 @@ int main(int argc,char** argv)
 			cout << i << " n'est pas divisible par "<< d << endl;
 		}
 		i=i+1;
+		*/
+
+		// m2mo_tp1_question_5
+		int e_quotient  = (int) i/d ;
+		int e_remainder =       i%d ;
+		cout << i << " = "<< e_quotient <<"*" <<d <<" + "<< e_remainder << endl;
+		++i;
 	}
+
+	return 0;
 }
