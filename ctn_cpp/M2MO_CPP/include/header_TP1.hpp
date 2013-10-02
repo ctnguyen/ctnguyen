84a29def0075a5fff2e0dae2f4bc8b27d0dcd473
chithanhnguyen.math <chithanhnguyen.math@gmail.com>
2013-10-02 13:14:34 +0000

    M2MO add new files for cpp project for M2MO

---
+++ ctn_cpp/M2MO_CPP/include/header_TP1.hpp
@@ -0,0 +1,17 @@
+
+/*!
+ *	@file include/header_TP1.hpp
+ *	@date Sept 2013
+ */
+
+
+#ifndef M2MO_HEADER_TP1_H
+#define M2MO_HEADER_TP1_H
+
+/*! \brief all declaration functions of tp1 will be declared here
+ * 
+ */
+
+
+
+#endif /* M2MO_HEADER_TP1_H */
