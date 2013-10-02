70cb6cd932102c6606c61b7e7ab3ce2d782fffe1
chithanhnguyen.math <chithanhnguyen.math@gmail.com>
2013-10-02 14:24:20 +0000

    m2mo first built with cmake the project cpp for m2mo

---
+++ ctn_cpp/M2MO_CPP/prog/TP1/exercice1.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+
+
+int main(int argc,char** argv)
+{
+	int d; // une variable entiere d
+	int i;
+	cout<<"****Premier programme****"<<endl;
+	cout<<"Saisissez un nombre puis appuyez sur entree : ";
+	cin >> d;
+	i=0; // on met 0 dans i
+	while(i<=15)
+	{
+		if( i % d == 0)
+		{
+			cout << i << " est divisible par "<< d << endl;
+		}
+		else
+		{
+			cout << i << " n'est pas divisible par "<< d << endl;
+		}
+		i=i+1;
+	}
+}
