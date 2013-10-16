76ab796670a13807be2d0f552708caeddcaa5bba
chithanhnguyen.math <chithanhnguyen.math@d822209c-5b66-c3cb-24dc-bd16cd606af5>
2013-10-16 09:53:51 +0000

    m2mo cpp add excercice2 for tp2

---
+++ trunk/ctn_cpp/M2MO_CPP/prog/TP2/exercice2.cpp
@@ -0,0 +1,22 @@
+/************************************************
+*                                               *
+*   C++ PROGRAMMING                             *
+*   MASTER LAURE ELIE 2013-2014                 *
+*   Student Chi Thanh NGUYEN                    *
+*                                               *
+*   chithanhnguyen.math@gmail.com               *
+*************************************************/
+
+#include<iostream>
+#include <cstdlib>
+
+using namespace std;
+
+
+int main(int argc,char** argv)
+{
+
+	std::cout<<"hello exercice 1 tp2 "<<std::endl;
+
+	return 0;
+}
