74ad5efae0ac944d663a4e6a1010eb467e3d5c63
chithanhnguyen.math <chithanhnguyen.math@d822209c-5b66-c3cb-24dc-bd16cd606af5>
2013-10-16 10:52:44 +0000

    m2mo cpp finish excercice2 for tp2

---
+++ ctn_cpp/M2MO_CPP/include/header_tp2.hpp
@@ -0,0 +1,33 @@
+/************************************************
+ *                                               *
+ *   C++ PROGRAMMING                             *
+ *   MASTER LAURE ELIE 2013-2014                 *
+ *   Student Chi Thanh NGUYEN                    *
+ *                                               *
+ *   chithanhnguyen.math@gmail.com               *
+ *************************************************/
+
+/*!
+ *	@file include/header_tp2.hpp
+ *	@date 16 Oct 2013
+ */
+
+
+#ifndef M2MO_HEADER_TP2_H
+#define M2MO_HEADER_TP2_H
+
+/*! \brief all declaration functions of tp1 will be declared here
+ * 
+ */
+namespace TP2
+{
+
+/*! ==  Exercice 2  ==
+ *
+ * \brief Print adress of a void pointer
+ */
+void adress(const void *a);
+
+} // End TP2
+
+#endif /* M2MO_HEADER_TP2_H */
