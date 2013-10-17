a136bbfeaa733fdeb4a75b261b4d417cf21c8663
chithanhnguyen.math <chithanhnguyen.math@d822209c-5b66-c3cb-24dc-bd16cd606af5>
2013-10-17 20:09:56 +0000

    m2mo cpp add initialization for test and more rich cmake configuration

--- trunk/ctn_cpp/M2MO_CPP/config.cmake
+++ trunk/ctn_cpp/M2MO_CPP/config.cmake
@@ -7,22 +7,28 @@
 #   chithanhnguyen.math@gmail.com               #
 #################################################
 
-# Configurations for cmake build
-# DO NOT modify this file
-# If you want to change the build options, copy this file to a file named exactly user-config.cmake
+#---------------------------#
+# DO NOT MODIFY THIS FILE   #
+#---------------------------# 
+
+# Configurations for cmake build, do not modify this file
+# If you want to change the build options, copy this file to a file named exactly "user-config.cmake"
 # DO NOT add user-config.cmake to svn repository
-# Modify options in the user-config.cmake and redo the command cmake for generating the project,
+# Modify options in the user-config.cmake and TYPE the command cmake for generating the project (see README.txt)
 
 
-
-
-
-
-# Choose your build type 
+### Choose your build type 
+###
 set(CMAKE_BUILD_TYPE Debug)
 #set(CMAKE_BUILD_TYPE Release)
 
+### Uncomment this line if you want to build tests , only IF YOU HAVE BOOTS
+###
+set(M2MO_BUILD_TEST TRUE)
 
-## Uncomment this line if you want to build tests , only IF YOU HAVE BOOTS
-#add_definitions(-DM2MO_BUILD_TEST)
-
+### If you have to build cpp project with a special compiler, IDE, 
+### Search the document for it about your cmake_generator, 
+### Uncomment these two lines and modify "your_special_cmake_generator" 
+###
+#set(M2MO_USER_DEFINE_GENERATOR)
+#set(CMAKE_GENERATOR "your_special_cmake_generator")
