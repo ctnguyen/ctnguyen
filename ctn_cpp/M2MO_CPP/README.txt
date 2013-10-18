eb4ca2ed850cd8ca9ffb4bd4da894b021dbba06b
chithanhnguyen.math <chithanhnguyen.math@d822209c-5b66-c3cb-24dc-bd16cd606af5>
2013-10-18 20:11:57 +0000

    m2mo cpp change config.cmake to default-config.cmake

--- ctn_cpp/M2MO_CPP/README.txt
+++ ctn_cpp/M2MO_CPP/README.txt
@@ -6,8 +6,8 @@ Info : Go to ENT of Paris Diderot, search C++
 Instruction building project with cmake :
 
 - Assuming user have downloaded all the sources code contained in a folder naming "src_m2mo"
-- There are a file "config.cmake" in the root folder, DO NOT modify it
-- Copy "config.cmake" to a file named exactly "user-config.cmake", modify this last adapting to your choice
+- There are a file "default-config.cmake" in the root folder, DO NOT modify it
+- Copy "default-config.cmake" to a file named exactly "user-config.cmake", modify this last adapting to your choice
 - Create beside it a folder named "build_m2mo". Go in to the folder "build_m2mo" and execute the command
 
 prompt$> cmake ../src_m2mo  # build project
--- ctn_cpp/M2MO_CPP/test/unit/test_lib_tp1.cpp
+++ ctn_cpp/M2MO_CPP/test/unit/test_lib_tp1.cpp
@@ -4,6 +4,7 @@
 #include <boost/test/included/unit_test.hpp>
 #include <boost/test/floating_point_comparison.hpp>
 
+#include <cstdlib>
 #include <header_tp1.hpp>
 
 BOOST_AUTO_TEST_SUITE(M2MO_lib_tp1)
@@ -17,6 +18,29 @@ BOOST_AUTO_TEST_CASE(test_pgcd_r)
 	BOOST_CHECK( TP1::pgcd_r(10,30) == 10 );
 	BOOST_CHECK( TP1::pgcd_r(28,12) ==  4 );
 }
+
+
+BOOST_AUTO_TEST_CASE(test_uniform)
+{
+	// sample some hundred of time a pair of number
+	// if each time the pair of number is the same, that is a bad sampling
+	// if the number of bad sampling is too hight, the test will failed
+	const int min=0 ; const int max = 100;
+	unsigned int nb_sample=200;
+	unsigned int bad_ramdom=0;
+
+	srand (time(NULL)); // IMPORTANT reinitialize random seed
+	int a1 = TP1::uniform(min,max,false);
+	for(unsigned int i=0;i<nb_sample;++i)
+	{
+		int a2 = TP1::uniform(min,max,false);
+		if(a1 == a2) ++bad_ramdom;
+		a1=a2;
+	}
+
+	BOOST_CHECK( bad_ramdom >= nb_sample );
+}
+
 
 
 BOOST_AUTO_TEST_SUITE_END()
