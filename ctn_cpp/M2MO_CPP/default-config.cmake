a0d1eb0f16bf3bfc7c39b7aaab387c1129d21c6f
chithanhnguyen.math <chithanhnguyen.math@d822209c-5b66-c3cb-24dc-bd16cd606af5>
2013-10-24 22:17:57 +0000

    m2mo cpp add a template file for cmake, helping to create an absolute path for output files in execution time

--- ctn_cpp/M2MO_CPP/default-config.cmake
+++ ctn_cpp/M2MO_CPP/default-config.cmake
@@ -17,6 +17,8 @@
 # Modify options in the user-config.cmake and TYPE the command cmake for generating the project (see README.txt)
 
 
+set(M2MO_HEAD_MESSAGE " [M2MO_Project]  =======  ") 
+
 ### Choose your build type 
 ###
 set(CMAKE_BUILD_TYPE Debug)
@@ -32,3 +34,10 @@ set(M2MO_BUILD_TEST TRUE)
 ###
 #set(M2MO_USER_DEFINE_GENERATOR)
 #set(CMAKE_GENERATOR "your_special_cmake_generator")
+
+### Path using for storing the output file 
+### You can choose an other path by commenting the first line and adapt the second 
+### NEVER choose to store the outputs file into the sources directory 
+###
+set(M2MO_OUTPUT_PATH "${CMAKE_BINARY_DIR}/ouput/")
+#set(M2MO_OUTPUT_PATH "choose_your_appropriated_path")
