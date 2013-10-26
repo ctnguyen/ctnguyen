4c235700212f509f58711b71f0f7022c05a74ca9
chithanhnguyen.math <chithanhnguyen.math@d822209c-5b66-c3cb-24dc-bd16cd606af5>
2013-10-26 20:44:31 +0000

    add R graphics book

--- trunk/ctn_R/M2MO_DATAMINING/exec/TP1_MCO_Multiple.R
+++ trunk/ctn_R/M2MO_DATAMINING/exec/TP1_MCO_Multiple.R
@@ -6,8 +6,15 @@
 ## i.e ".." from this file
 
 tab=read.table("data/immo.txt", header=TRUE, sep=";") # Read the data file from the data folder
-X= tab[c("surface","valeur")]; # Assign the X variable
-Y= tab[c("transaction")]     ; # Assign the Y variable
+
+## start by plotting data
+nb_obs = dim(tab)[2]-1
+for(i in 1:nb_obs ) {
+  plot(tab[,i],tab[,i+1],xlab=names(tab[i]),ylab=names(tab[nb_obs+1]))
+}
+
+X<-tab[c("surface","valeur")]; # Assign the X variable
+Y<-tab[c("transaction")]     ; # Assign the Y variable
 resmco=lsfit(X,Y)            ; # Least Squares Fit
 summary(resmco)              ;
 ## Reponse : see help(lsfit) for result explaination
@@ -16,6 +23,9 @@ summary(resmco)              ;
 resmco$coefficients[c(2,3)]
 ## After the lsfit result, if surface increase 1m2, the transaction increase 2.63439962, i.e resmco$coefficients["surface"]
 
+## Computation residual sum, verify if it is null
+sum_residual<-sum(resmco$residuals)
+
 ## Computation Mean Square Error
 MSE<- (1./length(resmco$residuals))*(resmco$residuals %*% resmco$residuals)
 sqrt_MSE<-sqrt(MSE)
@@ -23,7 +33,8 @@ sqrt_MSE<-sqrt(MSE)
 ## Computation Relative Errors
 relative_error<- resmco$residuals / tab$transaction
 max_rel_error<-max(abs(relative_error))
-#plot(relative_error)
+plot(relative_error)
+plot(abs(relative_error))
 ## The max relative error is about 8% which is relatively acceptable
 
 mean_transaction<-mean(tab$transaction)
