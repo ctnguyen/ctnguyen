312be2f6cd213dec3a609b37005aad82b33f874c
chithanhnguyen.math <chithanhnguyen.math@d822209c-5b66-c3cb-24dc-bd16cd606af5>
2013-10-27 09:34:00 +0000

    R add R graphics cookbook

--- trunk/ctn_R/M2MO_DATAMINING/exec/TP1_MCO_Multiple.R
+++ trunk/ctn_R/M2MO_DATAMINING/exec/TP1_MCO_Multiple.R
@@ -8,10 +8,8 @@
 tab=read.table("data/immo.txt", header=TRUE, sep=";") # Read the data file from the data folder
 
 ## start by plotting data
-nb_obs = dim(tab)[2]-1
-for(i in 1:nb_obs ) {
-  plot(tab[,i],tab[,i+1],xlab=names(tab[i]),ylab=names(tab[nb_obs+1]))
-}
+plot(tab[,1],tab[,3],xlab=names(tab[1]),ylab=names(tab[3]))
+plot(tab[,2],tab[,3],xlab=names(tab[2]),ylab=names(tab[3]))
 
 X<-tab[c("surface","valeur")]; # Assign the X variable
 Y<-tab[c("transaction")]     ; # Assign the Y variable
@@ -37,14 +35,19 @@ plot(relative_error)
 plot(abs(relative_error))
 ## The max relative error is about 8% which is relatively acceptable
 
+## analyse residuals
 mean_transaction<-mean(tab$transaction)
-#plot(resmco$residuals,tab$transaction)
-#plot(tab$transaction,resmco$residuals)
+fitted_transaction<-as.matrix(tab[c("surface","valeur")]) %*% resmco$coefficients[c("surface","valeur")] + resmco$coefficients["Intercept"]
+plot(fitted_transaction, resmco$residuals, xlab="fitted transaction",ylab="residuals")
+plot(fitted_transaction, abs(resmco$residuals), xlab="fitted transaction",ylab="abs residuals")
+## By plotting the residual and absolute value of residual in function of fitted data,
+## We verify if the residuals badly variate in function of magnitude of data
+## In the case of this exercice, non, residual behave seemingly good
+##
 ## By computing the mean transaction, supposing that the mean residual is zero
 ## then plotting graphic of residual and transaction, we see that the these two variables are correlated
 
-estimated_transaction<-as.matrix(tab[c("surface","valeur")]) %*% resmco$coefficients[c("surface","valeur")]
 
-plot(tab$transaction,estimated_transaction,xlim=c(0,1000),ylim=c(0,1000))
+plot(fitted_transaction, tab$transaction, ylim=c( max(tab$transaction),min(tab$transaction) ),xlim=c( max(fitted_transaction),min(fitted_transaction) ), ylab="transaction",xlab="fitted transaction")
 abline(0,pi/4)
 
