e08c71f2e02ddf8e3fc0a45e880db7ac0bb71549
chithanhnguyen.math <chithanhnguyen.math@gmail.com>
2014-05-22 13:40:02 +0000

    cpp LUNA little change in functions signatures on NumericalMethods

--- ctn_cpp/src_LUNA/LMM/numeric/NumericalMethods.h
+++ ctn_cpp/src_LUNA/LMM/numeric/NumericalMethods.h
@@ -16,40 +16,24 @@ class NumericalMethods
 {
 public:
 	
-	static double d1(double fwd,
-		double strike,
-		double vol,
-		double T);
+	static double d1(const double& fwd, const double& strike, const double& vol, const double& T);
 
-	static double d2(double fwd,
-		double strike,
-		double vol,
-		double T);
+	static double d2(const double& fwd, const double& strike, const double& vol, const double& T);
 
 	// Compute a derivative's price using Black's formula
-	static double Black_Price(double fwd,
-		double strike,
-		double vol,
-		double T);
+	static double Black_Price(const double& fwd, const double& strike, const double& vol, const double& T);
 
-	static double Black_Vega(double fwd,
-		double strike,
-		double vol,
-		double T); 
+	static double Black_Vega(const double& fwd, const double& strike, const double& vol, const double& T);
 
-	static double Black_Volga(double fwd,
-		double strike,
-		double vol,
-		double T);
+	static double Black_Volga(const double& fwd, const double& strike, const double& vol, const double& T);
 
-	//double impliedVolatility(double bs_call_price, 							  
-	//						 double strike, 
-	//						 double fwd,
-	//						 double T);
+	//double impliedVolatility(const double& bs_call_price, const double& strike, const double& fwd, const double& T);
 
-	double linearInterpolation(double t, 
+	double linearInterpolation(
+							   const double& t, 
 		                       const std::vector<double>& maturities,
-		                       const std::vector<double>& set_of_points);
+		                       const std::vector<double>& set_of_points
+							   );
 
 	//--------- TESTS ----------//
 	//void test_interpolator();
