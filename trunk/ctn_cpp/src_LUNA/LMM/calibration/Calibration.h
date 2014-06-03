b929fcd32b89f592190fde750d8dc66da93b2faf
chithanhnguyen.math@gmail.com <chithanhnguyen.math@gmail.com@d822209c-5b66-c3cb-24dc-bd16cd606af5>
2014-06-03 15:04:09 +0000

    cpp LUNA activate Calibration class

--- trunk/ctn_cpp/src_LUNA/LMM/calibration/Calibration.h
+++ trunk/ctn_cpp/src_LUNA/LMM/calibration/Calibration.h
@@ -1,21 +1,30 @@
-//#pragma once
-//
-//#include <boost\function.hpp>
-//#include <boost\bind.hpp>
-//#include <ql\math\optimization\endcriteria.hpp>
-//#include <ql\math\optimization\constraint.hpp>
-//#include <ql\math\optimization\problem.hpp>
-//#include "SwaptionCostFunction.h"
+#ifndef LMM_CALIBRATION_CALIBRATION_H
+#define LMM_CALIBRATION_CALIBRATION_H
+#pragma once
+
+#include <boost/function.hpp>
+#include <boost/bind.hpp>
+#include <ql/math/optimization/endcriteria.hpp>
+#include <ql/math/optimization/constraint.hpp>
+#include <ql/math/optimization/problem.hpp>
+
 //#include "TestCostFunction.h"
-//
-//class Calibration 
-//{	
-//public:
-//	Calibration();
-//	Array Calibrate(ofstream& outputFile, SwaptionCostFunction& func, const Array& startValues);
-//	void saveCalibrationResults(ofstream& outputFile, const Array& calibrationResults);
-//
-//
-//	//-- Tests --//
-//	void test_Minimizer(TestCostFunction&);
-//};
+#include <LMM/calibration/SwaptionApproxCostFunction.h>
+
+
+class Calibration 
+{	
+public:
+	
+	Calibration();
+	
+	Array Calibrate(ofstream& outputFile, SwaptionCostFunction& func, const Array& startValues);
+
+	void saveCalibrationResults(ofstream& outputFile, const Array& calibrationResults);
+
+
+	//-- Tests --//
+	//void test_Minimizer(TestCostFunction&);
+};
+
+#endif /* LMM_CALIBRATION_CALIBRATION_H */
