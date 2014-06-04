ed070ade8cd97918eeb3273e8c2596530fbe3dca
chithanhnguyen.math@gmail.com <chithanhnguyen.math@gmail.com@d822209c-5b66-c3cb-24dc-bd16cd606af5>
2014-06-04 08:39:13 +0000

    cpp LUNA fix test Tenor

--- trunk/ctn_cpp/src_LUNA/test/unit/test_Tenor.cpp
+++ trunk/ctn_cpp/src_LUNA/test/unit/test_Tenor.cpp
@@ -14,31 +14,31 @@ BOOST_AUTO_TEST_CASE(test_TenorType_constructors)
 
 	Tenor TenorNon = Tenor::_Non;
 	BOOST_CHECK(TenorNon.nbMonth==0) ; std::cout<< TenorNon <<std::endl;
-	BOOST_CHECK(strcmp(TenorNon.name, "_Non")==0);
+	BOOST_CHECK(strcmp(TenorNon.name, "Non")==0);
 
 	Tenor Tenor1M  = Tenor::_1M;
 	BOOST_CHECK( Tenor1M.nbMonth == 1 ) ; std::cout<< Tenor1M <<std::endl;
-	BOOST_CHECK(strcmp( Tenor1M.name , "_1M" )==0);
+	BOOST_CHECK(strcmp( Tenor1M.name , "1M" )==0);
 
 	Tenor Tenor3M  = Tenor::_3M;
 	BOOST_CHECK( Tenor3M.nbMonth == 3 ); ; std::cout<< Tenor3M <<std::endl;
-	BOOST_CHECK(strcmp( Tenor3M.name , "_3M" )==0);
+	BOOST_CHECK(strcmp( Tenor3M.name , "3M" )==0);
 
 	Tenor Tenor6M  = Tenor::_6M;
 	BOOST_CHECK( Tenor6M.nbMonth == 6 ) ; std::cout<< Tenor6M <<std::endl;
-	BOOST_CHECK(strcmp( Tenor6M.name , "_6M" )==0);
+	BOOST_CHECK(strcmp( Tenor6M.name , "6M" )==0);
 
 	Tenor Tenor9M  = Tenor::_9M;
 	BOOST_CHECK( Tenor9M.nbMonth == 9 ) ; std::cout<< Tenor9M <<std::endl;
-	BOOST_CHECK(strcmp( Tenor9M.name , "_9M" )==0);
+	BOOST_CHECK(strcmp( Tenor9M.name , "9M" )==0);
 
 	Tenor Tenor12M = Tenor::_12M;
 	BOOST_CHECK( Tenor12M.nbMonth == 12 ) ; std::cout<< Tenor12M <<std::endl;
-	BOOST_CHECK(strcmp( Tenor12M.name , "_12M" )==0);
+	BOOST_CHECK(strcmp( Tenor12M.name , "12M" )==0);
 
 	Tenor Tenor1Y  = Tenor::_1Y;
 	BOOST_CHECK( Tenor1Y.nbMonth == 12 ) ; std::cout<< Tenor1Y <<std::endl;
-	BOOST_CHECK(strcmp( Tenor1Y.name , "_1Y" )==0);
+	BOOST_CHECK(strcmp( Tenor1Y.name , "1Y" )==0);
 }
 
 BOOST_AUTO_TEST_CASE(test_TenorType_comparing)
