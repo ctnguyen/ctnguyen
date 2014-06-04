61ac11e58761e091207db27aa6308553fa3f15c5
chithanhnguyen.math <chithanhnguyen.math@gmail.com>
2014-06-04 17:13:06 +0000

    cpp LUNA : - debug and correctly rewrite MarketInfoCollector - add reading Tenors and Maturities in MarketInfoCollector

--- ctn_cpp/src_LUNA/LMM/helper/MarketInfoCollector.h
+++ ctn_cpp/src_LUNA/LMM/helper/MarketInfoCollector.h
@@ -7,31 +7,58 @@
 #include <fstream>
 #include <vector>
 
-typedef std::vector< std::vector<double> > matrix_;
 
+/* \class MarketInfocollector read all info from an ATM Swaption Data into numerical data
+ *
+ *
+ */
 class MarketInfoCollector
 {
-private:
-	static matrix_ swapRates_;
-	static matrix_ swaptionVolatilityMatrix_;
-	static std::vector<double> libors_;
-	static std::vector<double> zeroCouponBonds_;
-	static std::vector<double> zcMaturities_;
-
 public:
+	typedef std::vector< std::vector<double> > matrix_;
 
 	static void readMarketInfo(const std::string& fileIn);
 		                
 	//-- Getters --//
-	static matrix_& get_swapRates();
-	static matrix_& get_swaptionVolatilityMatrix();
-	static std::vector<double>& get_libors();
-	static std::vector<double>& get_zcBonds();
-	static std::vector<double>& get_zcMaturities();
+	static std::vector<double>& get_swpnTerms()      ;
+	static std::vector<double>& get_swpnMaturities() ;
+	static matrix_& get_swapRates()                  ; 
+	static matrix_& get_swaptionVolatilityMatrix()   ;
 
-	// Convert string to double written with '.'
-	static double toDouble(std::string s);
+	static std::vector<double>& get_libors()       ;
+	static std::vector<double>& get_zcBonds()      ;
+	static std::vector<double>& get_zcMaturities() ;
 
+
+	//print all data to given file
+	static void print(const std::string& filename);
+
+private:
+
+	static std::vector<double> libors_           ;
+	static std::vector<double> zeroCouponBonds_  ;
+	static std::vector<double> zcMaturities_     ;
+	
+	static std::vector<double> swpnTerms_        ;
+	static std::vector<double> swpnMaturities_   ;
+
+	static matrix_ swaptionVolatilityMatrix_     ; // volatilities matrix
+	static matrix_ swapRates_                    ; // strike matrix at ATM Swaption
+	
+	
+
+
+	// Convert string to appropriated double value 
+	static double numerical_value(std::string s);
+
+	// check if case is a Tenor, containing text as YR, MO, ...
+	static bool isTenor(const std::string& s);
+
+	// check if case is just label text as pillar, P, L, Expiry, ##### , empty case ...
+	static bool isNotUsefulCase(const std::string& s); 
+
+	// The second line of Excel file containe information about Swaption Terms
+	static void parseTermsLine(const std::string& firstLine);
 };
 
 
