a30a29c5498f68d59b9f375e37786a981ff5ce7b
chithanhnguyen.math@gmail.com <chithanhnguyen.math@gmail.com@d822209c-5b66-c3cb-24dc-bd16cd606af5>
2014-06-04 17:13:06 +0000

    cpp LUNA : - debug and correctly rewrite MarketInfoCollector - add reading Tenors and Maturities in MarketInfoCollector

--- trunk/ctn_cpp/src_LUNA/src/helper/MarketInfoCollector.cpp
+++ trunk/ctn_cpp/src_LUNA/src/helper/MarketInfoCollector.cpp
@@ -5,15 +5,33 @@
 #include <cstdlib>
 #include <algorithm>
 
+#include <LMM/generic_path.h>
+
 using namespace std;
 
 
 //-- Init static attributes
-matrix_ MarketInfoCollector::swapRates_;
-matrix_ MarketInfoCollector::swaptionVolatilityMatrix_;
+std::vector<double> MarketInfoCollector::swpnTerms_            ;
+std::vector<double> MarketInfoCollector::swpnMaturities_       ;
+
+MarketInfoCollector::matrix_ MarketInfoCollector::swapRates_               ;
+MarketInfoCollector::matrix_ MarketInfoCollector::swaptionVolatilityMatrix_;
+
+std::vector<double> MarketInfoCollector::libors_          ;
 std::vector<double> MarketInfoCollector::zeroCouponBonds_ ;
-std::vector<double> MarketInfoCollector::libors_;
-std::vector<double> MarketInfoCollector::zcMaturities_;
+std::vector<double> MarketInfoCollector::zcMaturities_    ;
+
+
+std::vector<double>& MarketInfoCollector::get_swpnTerms()      { return swpnTerms_; }
+std::vector<double>& MarketInfoCollector::get_swpnMaturities() { return swpnMaturities_; }
+
+MarketInfoCollector::matrix_& MarketInfoCollector::get_swapRates(){return swapRates_;}
+MarketInfoCollector::matrix_& MarketInfoCollector::get_swaptionVolatilityMatrix(){return swaptionVolatilityMatrix_;}
+std::vector<double>& MarketInfoCollector::get_libors(){return libors_;}
+std::vector<double>& MarketInfoCollector::get_zcBonds(){return zeroCouponBonds_;}
+std::vector<double>& MarketInfoCollector::get_zcMaturities(){return zcMaturities_;}
+
+
 
 
 void MarketInfoCollector::readMarketInfo(const std::string& fileIn)	                    
@@ -24,12 +42,14 @@ void MarketInfoCollector::readMarketInfo(const std::string& fileIn)
 	matrix_ result;
 	string line;
 	char delimiter = '#';
-	
+
 	if (instream.is_open())
 	{
 		//-- Ignore the first two lines of xls file
 		getline (instream,line);
 		getline (instream,line);
+
+		parseTermsLine(line);
 
 		while (instream.good())
 		{
@@ -37,52 +57,37 @@ void MarketInfoCollector::readMarketInfo(const std::string& fileIn)
 			stringstream ss(line); 
 
 			//-- If current swaption row has char '#', stop reading
-			if (line.at(8) == delimiter)
-				break;
+			if (line.at(0) == ';' && line.at(6) ==  delimiter) break;
 
 			string s;
 			vector<double> res;
-			
+
 			//-- Loop on each cell of the current line
 			while(getline(ss,s,';'))
 			{
-				double convertedString = toDouble(s); // to handle float format in excel
-
-				//-- Convert ZC maturities to year fractions
-				if (s.find('D') != std::string::npos)
-				{
-					convertedString /= 360.;
-				}
-				else if (s.find('W') != std::string::npos)
-				{
-					convertedString /= 72.;
-				}
-				else if (s.find('M') != std::string::npos)
-				{
-					convertedString /= 12.;
-				}
+				double convertedString = numerical_value(s); // to handle float format in excel
 
 				res.push_back(convertedString);
 			}
+
+			// pop back zeros at the en of reading line
+			while (!res.empty() && res.back() == 0.0 ) res.pop_back();
+
 			result.push_back(res);
 		}
 	}
 	else
 	{
-		cout << ".. hello hello ... Market Info Collector: --> Cannot open requested file" << endl;
-		exit(0);
+		cout << ".. hello hello ... Market Info Collector: --> Cannot open requested file" << endl; 	exit(0);
 	}
 
 	for (size_t i = 0; i < result.size(); ++i)
 	{
-		if (result[i][1] != 0.)
-			libors_.push_back(result[i][1]);
+		if (result[i][1] != 0.) libors_.push_back(result[i][1]);         // second column
 
-		if (result[i][3] != 0.)
-			zcMaturities_.push_back(result[i][3]);
+		if (result[i][3] != 0.)	zcMaturities_.push_back(result[i][3]);   // 4th column
 
-		if (result[i][4] != 0.)
-			zeroCouponBonds_.push_back(result[i][4]);
+		if (result[i][4] != 0.)	zeroCouponBonds_.push_back(result[i][4]);// 5th column		
 	}
 
 	//-- Swaption matrix
@@ -93,7 +98,10 @@ void MarketInfoCollector::readMarketInfo(const std::string& fileIn)
 		{
 			swaptionmatrix_tmp.push_back(result[i][j]);
 		}
+
 		swaptionVolatilityMatrix_.push_back(swaptionmatrix_tmp);
+
+		swpnMaturities_.push_back(result[i][6]);             // 7th column
 	}
 
 	//-- Swap rates
@@ -112,15 +120,119 @@ void MarketInfoCollector::readMarketInfo(const std::string& fileIn)
 }
 
 
-double MarketInfoCollector::toDouble(std::string s)
+double MarketInfoCollector::numerical_value(std::string s)
 {
-    std::replace(s.begin(), s.end(), ',', '.');
-    return std::atof(s.c_str());
+	if ( isNotUsefulCase(s) ) return 0;
+
+	if ( isTenor(s) )
+	{
+		size_t foundYR = s.find("YR");
+		if(foundYR != std::string::npos ) 
+		{
+			s.erase(foundYR, 2);          // if found "YR", remove two characters
+			return std::atof(s.c_str());  // the string left is tenor in year unit
+		}
+
+		size_t foundMO = s.find("MO");
+		if(foundMO != std::string::npos ) 
+		{
+			s.erase(foundMO, 2);             // if found "MO", remove two characters
+			return std::atof(s.c_str())/12.; // the string left is tenor in month unit
+		}
+
+		//size_t foundD = s.find("W");
+		//if(foundD != std::string::npos ) 
+		//{
+		//	s.erase(foundD, 1);              // if found "W", remove 1 characters
+		//	return std::atof(s.c_str())/72.; // the string left is tenor in week unit
+		//}
+
+		//size_t foundD = s.find("D");
+		//if(foundD != std::string::npos ) 
+		//{
+		//	s.erase(foundD, 1);               // if found "D", remove 1 characters
+		//	return std::atof(s.c_str())/360.; // the string left is tenor in day unit
+		//}
+	}
+
+	std::replace(s.begin(), s.end(), ',', '.');
+	return std::atof(s.c_str());
 }
 
-matrix_& MarketInfoCollector::get_swapRates(){return swapRates_;}
-matrix_& MarketInfoCollector::get_swaptionVolatilityMatrix(){return swaptionVolatilityMatrix_;}
-std::vector<double>& MarketInfoCollector::get_libors(){return libors_;}
-std::vector<double>& MarketInfoCollector::get_zcBonds(){return zeroCouponBonds_;}
-std::vector<double>& MarketInfoCollector::get_zcMaturities(){return zcMaturities_;}
+bool MarketInfoCollector::isTenor(const std::string& s)
+{
+	// if text contain 'Y' or 'M' , that is a tenor 
+	return (s.find('Y') != std::string::npos || s.find('M') != std::string::npos ) ;
+}
 
+bool MarketInfoCollector::isNotUsefulCase(const std::string& s)
+{
+	return (s.empty()||s[0] ==' '|| s[0] == 'p' || s[0] == 'L' || s[0] == 'P' || s[0] == 'E' || s[0] == '#');
+}
+
+void MarketInfoCollector::parseTermsLine(const std::string& firstLine)
+{
+	stringstream ss(firstLine); 
+
+	string s;
+	
+	//-- Loop on each cell of the current line
+	while(getline(ss,s,';'))
+	{
+		double term = numerical_value(s); // to handle float format in excel
+
+		if(term != 0.)	swpnTerms_.push_back(term);
+	}
+}
+
+
+
+
+void MarketInfoCollector::print(const std::string& filename)
+{
+	std::string path_FileOut = LMM::get_output_path() + filename;
+
+	std::ofstream outputFile  ;  outputFile.open(path_FileOut.c_str() );
+
+	outputFile << "- LIBORS ---" << std::endl;
+	for (auto lib_bb : libors_) outputFile <<" "<< lib_bb << ";";
+	outputFile << std::endl << std::endl;
+
+	outputFile << "- ZC BONDS ---" << std::endl;
+	for (auto zc_bb : zeroCouponBonds_) outputFile <<" "<<zc_bb << ";";
+	outputFile << std::endl << std::endl;
+
+	outputFile << "- ZC MATURITIES ---" << std::endl;
+	for (auto zc_m : zcMaturities_) outputFile <<" "<<zc_m << ";";
+	outputFile << std::endl << std::endl<< std::endl;
+
+
+
+	outputFile << "- SWPN TERMS ---" << std::endl;
+	for (auto swpn_t : swpnTerms_) outputFile <<" "<<swpn_t << ";";
+	outputFile << std::endl << std::endl;
+
+	outputFile << "- SWPN MATURITIES ---" << std::endl;
+	for (auto swpn_m : swpnMaturities_) outputFile <<" "<<swpn_m << ";";
+	outputFile << std::endl << std::endl;
+
+	outputFile <<  "=======    ATM SWAPTION VOL MATRIX   ========" << std::endl;
+	for (auto swpMatRow : swaptionVolatilityMatrix_)
+	{
+		for (auto swpVol : swpMatRow) outputFile <<" "<< swpVol << ";";
+		outputFile << std::endl;
+	}
+	outputFile << std::endl << std::endl;
+
+
+	outputFile << "=======    SWAP RATES   =========" << std::endl;
+	for (auto swpRateRow : swapRates_)
+	{
+		for (auto swpR : swpRateRow) outputFile <<" "<< swpR << ";";
+		outputFile << std::endl;
+	}
+	outputFile << std::endl;
+
+	outputFile.close();	
+
+}
--- trunk/ctn_cpp/src_LUNA/test/unit/test_helper_MarketInfoCollector.cpp
+++ trunk/ctn_cpp/src_LUNA/test/unit/test_helper_MarketInfoCollector.cpp
@@ -1,8 +1,6 @@
 #define BOOST_TEST_MODULE LMM_unit_test_helper_MarketInfoCollector
 #include <boost/test/included/unit_test.hpp>
 
-#include <string>
-
 #include <LMM/generic_path.h>
 
 #include <LMM/helper/MarketInfoCollector.h>
@@ -13,59 +11,63 @@ BOOST_AUTO_TEST_CASE(test_marketinfocollector)
 {
 	
 	std::string path_FileIn = LMM::get_runtime_datapath() + "Mkt_info_1.csv";
-	std::cout<< "File Input Paht :"<< path_FileIn << std::endl;
+	std::cout<< "Input ATM Swaption DataFile  :"<< path_FileIn << std::endl;
 	
 	MarketInfoCollector::readMarketInfo( path_FileIn );
 
 
-	std::vector<double> libors_BB = MarketInfoCollector::get_libors();
-	std::vector<double> zc_BB = MarketInfoCollector::get_zcBonds();
-	matrix_ swaptionMatrix_BB = MarketInfoCollector::get_swaptionVolatilityMatrix();
-	matrix_ swaprates_BB = MarketInfoCollector::get_swapRates();
+	std::vector<double> libors_BB     = MarketInfoCollector::get_libors();
+	std::vector<double> zc_BB         = MarketInfoCollector::get_zcBonds();
+	std::vector<double> zc_Maturities = MarketInfoCollector::get_zcMaturities();
+	
+	std::vector<double> swpnMaturities_BB          = MarketInfoCollector::get_swpnMaturities();
+	std::vector<double> swpnTerms_BB               = MarketInfoCollector::get_swpnTerms();
+	MarketInfoCollector::matrix_ swaprates_BB      = MarketInfoCollector::get_swapRates();
+	MarketInfoCollector::matrix_ swaptionMatrix_BB = MarketInfoCollector::get_swaptionVolatilityMatrix();
+	
+	BOOST_CHECK(libors_BB.size() == 4);
+	BOOST_CHECK(libors_BB[0] == 0.020);
+	BOOST_CHECK(libors_BB[1] == 0.023);
+	BOOST_CHECK(libors_BB[2] == 0.024);
+	BOOST_CHECK(libors_BB[3] == 0.026);
+
+	BOOST_CHECK(zc_BB.size() == 4);
+	BOOST_CHECK(zc_BB[0] == 0.99);
+	BOOST_CHECK(zc_BB[1] == 0.97);
+	BOOST_CHECK(zc_BB[2] == 0.95);
+	BOOST_CHECK(zc_BB[3] == 0.93);
+
+	BOOST_CHECK(zc_Maturities.size() == 4);
+	BOOST_CHECK(zc_Maturities[0] == 0.1);
+	BOOST_CHECK(zc_Maturities[1] == 0.2);
+	BOOST_CHECK(zc_Maturities[2] == 1.0);
+	BOOST_CHECK(zc_Maturities[3] == 2.0);
 
 
-	//-- Put all results in Excel
-	std::string path_FileOut = LMM::get_output_path() + "test_MarketInfoCollector.csv";
-	std::cout<< "File Output Path :"<< path_FileOut << std::endl;
-	std::ofstream outputFile;
-	outputFile.open(path_FileOut.c_str() );
+	BOOST_CHECK(swpnMaturities_BB.size() == 19 ); // test the Swaption Maturities the two extrem elements
+	BOOST_CHECK(swpnMaturities_BB[0]  == 1./12. ) ;
+	BOOST_CHECK(swpnMaturities_BB[4]  == 1.     ) ;
+	BOOST_CHECK(swpnMaturities_BB[18] == 30     ) ;
+	
+	BOOST_CHECK(swpnTerms_BB.size() == 15 ); // test the Swaption Terms the two extrem elements
+	BOOST_CHECK(swpnTerms_BB[0]  == 1  ) ; 
+	BOOST_CHECK(swpnTerms_BB[11] == 15 ) ;
+	BOOST_CHECK(swpnTerms_BB[14] == 30 ) ;
 
-	outputFile << "LIBORS" << std::endl;
-	for (auto lib_bb : libors_BB)
-		outputFile << lib_bb << ";";
-
-	outputFile << std::endl << std::endl;
-
-	outputFile << "ZC BONDS" << std::endl;
-	for (auto zc_bb : zc_BB)
-		outputFile << zc_bb << ";";
-
-	outputFile << std::endl << std::endl;
-
-	outputFile << "ATM SWAPTION VOL MATRIX" << std::endl;
-	for (auto swpMatRow : swaptionMatrix_BB)
-	{
-		for (auto swpVol : swpMatRow)
-			outputFile << swpVol << ";";
-
-		outputFile << std::endl;
-	}
-	outputFile << std::endl;
+	//check the 4 cases at corners of each matrices is enough
+	BOOST_CHECK(swaprates_BB.size()    == 19 ) ; // nb Lines  == 19
+	BOOST_CHECK(swaprates_BB[0].size() == 15 ) ; // nb Column == 15
+	BOOST_CHECK(swaprates_BB[0][0]  == 0.4196 ) ; BOOST_CHECK( swaprates_BB[0][14]  == 2.7584 );
+	BOOST_CHECK(swaprates_BB[18][0] == 2.8271 ) ; BOOST_CHECK( swaprates_BB[18][14] == 2.6174 );
 
 
-	outputFile << "SWAP RATES" << std::endl;
-	for (auto swpRateRow : swaprates_BB)
-	{
-		for (auto swpR : swpRateRow)
-			outputFile << swpR << ";";
+	BOOST_CHECK(swaptionMatrix_BB.size()    == 19 ) ; // nb Lines  == 19
+	BOOST_CHECK(swaptionMatrix_BB[0].size() == 15 ) ; // nb Column == 15
+	BOOST_CHECK(swaptionMatrix_BB[0][0]  == 25.35 ) ; BOOST_CHECK( swaptionMatrix_BB[0][14]  == 58.60 );
+	BOOST_CHECK(swaptionMatrix_BB[18][0] == 56.63 ) ; BOOST_CHECK( swaptionMatrix_BB[18][14] == 31.35 );
+	
 
-		outputFile << std::endl;
-	}
-	outputFile << std::endl;
-
-	outputFile.close();
-
-	BOOST_CHECK(true);
+	MarketInfoCollector::print("unitTest_MarketInfoCollector.csv");
 }
 
 BOOST_AUTO_TEST_SUITE_END()
