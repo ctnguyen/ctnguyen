30bb6b80061e7e5ba99ac97214dbbcf5cc5a4730
chithanhnguyen.math@gmail.com <chithanhnguyen.math@gmail.com@d822209c-5b66-c3cb-24dc-bd16cd606af5>
2014-06-09 22:17:41 +0000

    migrated LUNA project to a separated repository

--- trunk/ctn_cpp/src_LUNA/data/misc/deprecated_cpp/VolatilityModel.cpp
+++
@@ -1,6 +0,0 @@
-//#include "VolatilityModel.h"
-//
-//matrix<Real> VolatilityModel::getB(const VolatilityModel & myVolMod) { return myVolMod.B; }
-//
-//matrix<Real> VolatilityModel::getCorrMatrix(const VolatilityModel & myVolMod) { return myVolMod.corrMatrix; }
-//
--- trunk/ctn_cpp/src_LUNA/src/instrument/CMS.cpp
+++
@@ -1,2 +0,0 @@
-
-#include <LMM/instrument/CMS.h>
