a521ce48512132d0a2c919a3fa05c001bf0b081f
chithanhnguyen.math <chithanhnguyen.math@gmail.com>
2014-12-20 16:37:06 +0000

    add a counter-example for race condition, but not works for Ubuntu for instance

---
+++ ctn_cpp/cppx11/multi_thread/race_condition_ex.cpp
@@ -0,0 +1,45 @@
+/*! \author chithanhnguyen.math@gmail.com
+ *
+ *  20.Dec.2014
+ *
+ *  This is an counter-example of race condition
+ *  A class Oscillation is supposed to hold a integer value.
+ *
+ *  Invariant Condition of the class
+ *  Each time that call a oscillate method, this value oscillate, but
+ *  always return the state into zero
+ *
+ *  Two thread without mutex protection will break this invariant.
+ *
+ */
+
+#include <thread>
+#include <iostream>
+
+class Oscillation
+{
+public:
+
+	void oscillate(const int nb)
+	{
+		for(int i=0;i<nb;++i){ m_value+=1; }
+		for(int i=0;i<nb;++i){ m_value-=1; }
+	}
+
+	void print() const { std::cout<< "My Value "<<m_value<<std::endl;}
+private:
+	int m_value;
+};
+
+
+int main()
+{
+	Oscillation oscilator;
+	std::thread t1(&Oscillation::oscillate, &oscilator, 10 );
+	//std::thread t2(&Oscillation::oscillate, &oscilator, 10 );
+
+	t1.join();
+	//t2.join();
+
+	oscilator.print();
+}
