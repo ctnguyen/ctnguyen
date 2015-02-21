f068b39fafd21b2512e4c806bff6f83a96aae3c0
chithanhnguyen.math@gmail.com <chithanhnguyen.math@gmail.com@d822209c-5b66-c3cb-24dc-bd16cd606af5>
2015-02-21 20:40:06 +0000

    filled offline data build

--- trunk/ctn_cpp/tempo/colossus/DataStream.h
+++ trunk/ctn_cpp/tempo/colossus/DataStream.h
@@ -1,18 +1,67 @@
 /*!
  * DataStream.h
  */
-
-
 #ifndef DATASTREAM_H
 #define DATASTREAM_H
 
+#include <vector>
+#include <iostream>
+
+/*! Class define interface for DataStream
+ */
 class DataStream
 {
 public:
 
-	DataStream();
+	// check if the stream is ended
+	virtual bool isEnd() const = 0;
 
-	virtual ~DataStream(){}
+	// update the stream
+	virtual void update() =0;
+
+	virtual const std::vector<int>& getNewData() const = 0;	
 };
+
+
+/*! Class Simulate a Data Stream
+ *  Data can be read from a text file
+ *  Or generated randomly 
+ */
+class SampleDataStream : public DataStream
+{
+public:
+	
+	// build offline stream by reading a file
+	SampleDataStream(const char* filename);
+	
+	// build offline stream by randomly generating numbers
+	SampleDataStream(unsigned int N);
+
+	// check if the stream is ended
+	virtual bool isEnd() const;
+
+	// update the stream
+	virtual void update();
+
+	virtual const std::vector<int>& getNewData() const;
+
+	virtual ~SampleDataStream();
+
+	const std::vector<int>& get_OfflineData() const;
+
+	friend std::ostream& operator<<(std::ostream& os, const SampleDataStream& offlinestream);
+private :
+
+	size_t counter_;
+
+	std::vector<int> offline_data_;
+
+	std::vector<int> new_data_buffer;
+	
+	void write_to_stream(std::ostream& os) const;
+};
+
+//std::ostream& operator<<(std::ostream& os, const SampleDataStream& offlinestream);
+
 
 #endif /* DATASTREAM_H */
