730db208ec9e2f97e971f01aabfc24657d18fcd2
chithanhnguyen.math <chithanhnguyen.math@gmail.com>
2015-02-22 05:47:42 +0000

    cpp colossus add test for min-max container

--- ctn_cpp/tempo/colossus/helper.h
+++ ctn_cpp/tempo/colossus/helper.h
@@ -6,11 +6,27 @@
 
 #include <vector>
 #include <iostream>
+#include <queue>
 
 namespace helper
 {
 
+struct Greater
+{
+    bool operator() ( int i, int j)
+    {
+    	return i > j;
+    }
+} greater;
 
+}
+
+std::ostream& operator<<(std::ostream& os, const std::vector<int>& v)
+{
+	for(size_t i=0;i<v.size();++i)
+		std::cout<<v[i]<<"  ";
+
+	return os;
 }
 
 #endif /* HELPER_H */
