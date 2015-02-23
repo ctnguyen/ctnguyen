0a09a6dae4d13a8adcc451b09993d71e458eb364
chithanhnguyen.math@gmail.com <chithanhnguyen.math@gmail.com@d822209c-5b66-c3cb-24dc-bd16cd606af5>
2015-02-23 00:13:07 +0000

    cpp colossus : add FileStreamReader

--- trunk/ctn_cpp/tempo/colossus/DataStream.h
+++ trunk/ctn_cpp/tempo/colossus/DataStream.h
@@ -6,6 +6,7 @@
 
 #include <vector>
 #include <iostream>
+#include <fstream>
 
 /*! Class define interface for DataStream
  */
@@ -20,6 +21,41 @@ public:
 	virtual void fetchData() = 0;
 
 	virtual const std::vector<int>& get_DataBuffer() const = 0;	
+};
+
+
+/*! Class FileStreamReader
+*/
+class FileStreamReader : public DataStream
+{
+public:
+
+	// build offline stream by reading a file
+	// A small bug appear on Windows, 
+	// - sample.txt has to be the same folder of .exe if execute in commandline
+	// - sample.txt has to be the ${BINARY_DIR}\test if execute in debuger 
+	FileStreamReader(const char* filename, const unsigned int buffer_size = 1);
+
+	virtual ~FileStreamReader();
+
+	// check if the stream is ended
+	virtual bool isEnd() const;
+
+	// update the stream
+	virtual void fetchData();
+
+	// get the fetched data 
+	virtual const std::vector<int>& get_DataBuffer() const;
+
+private:
+
+	const unsigned int BUFFER_SIZE_;
+
+	std::vector<int> data_buffer_;
+
+	std::ifstream input_stream_;
+
+	bool isEndStream;
 };
 
 
@@ -64,6 +100,11 @@ private :
 	
 	void write_to_stream(std::ostream& os) const;
 };
+
+
+
+
+
 
 
 #endif /* DATASTREAM_H */
