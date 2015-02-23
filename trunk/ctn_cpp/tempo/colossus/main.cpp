c67daaaadff0a774636e9731c8384df15ae56932
chithanhnguyen.math@gmail.com <chithanhnguyen.math@gmail.com@d822209c-5b66-c3cb-24dc-bd16cd606af5>
2015-02-23 00:35:12 +0000

    cpp colossus : - write main program - fixe bug when fetched data buffer would be empty

--- trunk/ctn_cpp/tempo/colossus/main.cpp
+++ trunk/ctn_cpp/tempo/colossus/main.cpp
@@ -1,9 +1,39 @@
+#include <iostream>
+
+#include "helper.h"
 #include "DataStream.h"
+#include "Selection.h"
+#include "Median.h"
 
 
-int main()
+int main(int argc, char ** argv)
 {
-	SampleDataStream data(100);
+	const unsigned int k = 10;
+
+	const char* input_filename = "sample_data_1000.txt";
+	
+	if (argc == 2)
+	{
+		input_filename = argv[1];
+	}
+
+	Median median_tracker;
+	Selection min_set_tracker(k);
+	FileStreamReader file_reader(input_filename);
+
+	while (!file_reader.isEnd())
+	{
+
+		file_reader.fetchData();
+		const std::vector<int>& buffer = file_reader.get_DataBuffer();
+	
+		median_tracker.updateData(buffer);
+		min_set_tracker.updateData(buffer);
+
+		std::cout << "Actual median " << median_tracker.get_Median()<<"	";
+		std::cout << "Actual "<<k<<"-th order stat " << min_set_tracker.get_OrderStatistic(k)<<std::endl;
+	}
+	
 
 	return 0;
 }
---
+++ trunk/ctn_cpp/tempo/colossus/time_measure.cpp
@@ -0,0 +1,9 @@
+#include "DataStream.h"
+
+
+int main()
+{
+	SampleDataStream data(100);
+
+	return 0;
+}
