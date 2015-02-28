1fe802d1194eb4cc541613db0f550284e65330ae
chithanhnguyen.math@gmail.com <chithanhnguyen.math@gmail.com@d822209c-5b66-c3cb-24dc-bd16cd606af5>
2015-02-28 20:27:15 +0000

    first implementation of structure

--- trunk/ctn_cpp/tempo/OXAM/OXAM/OXAM.cpp
+++ trunk/ctn_cpp/tempo/OXAM/OXAM/OXAM.cpp
@@ -17,9 +17,9 @@
 class Order
 {
 public:
-	Order(unsigned long _time, unsigned int _orderID) :time(_time), orderID(_orderID){}
-	const unsigned long time;
-	const unsigned orderID;
+	Order(unsigned long _time = 0, unsigned int _orderID = 0) :time(_time), orderID(_orderID){}
+	unsigned long time;
+	unsigned orderID;
 	virtual bool isErease() const = 0 ;
 	virtual bool isInsert() const = 0;
 	virtual double price() const = 0;
@@ -31,7 +31,7 @@ std::ostream& operator<<(std::ostream& os, const Order& order);
 class EraseOrder : public Order
 {
 public:
-	EraseOrder(unsigned long _time, unsigned int _orderID) :Order(_time,_orderID){}
+	EraseOrder(unsigned long _time = 0, unsigned int _orderID = 0) :Order(_time, _orderID){}
 	virtual bool isErease() const { return true; };
 	virtual bool isInsert() const { return false; };
 	virtual double price() const { throw("EreaseOrder do not have price"); return -100000; }
@@ -40,30 +40,44 @@ public:
 class InsertOrder : public Order
 {
 public:
-	InsertOrder(unsigned long _time, unsigned int _orderID, const double& _price) 
+	InsertOrder(unsigned long _time = 0, unsigned int _orderID = 0, const double& _price = 0)
 	: Order(_time, _orderID), price_(_price){}
 	
 	virtual bool isErease() const { return false ; }
 	virtual bool isInsert() const { return true  ; }
 	virtual double price() const { return price_; }
+
+	InsertOrder(const InsertOrder& rhs)
+	: Order(rhs.time, rhs.orderID), price_(rhs.price() ) {}
+
+	InsertOrder& operator=(const InsertOrder& rhs)
+	{
+		time = rhs.time; orderID = rhs.orderID; price_ = rhs.price_;
+		return *this; 
+	}
 private:
 	double price_;
 };
 
-// Order Stream would be dynamic(online). 
+// InputOrderContainer would be dynamic(online). 
 // But for quick implementation, it is static(offline) and data is read from text file
-class OrderBook
+// InputOrderContainer's data should also be simulated for test
+class InputOrderContainer
 {
 public:
 
-	const double highestPrice()const{}
-	const double highestPrice()const{}
+	double highestPrice()const; 
 
 	const Order& operator[] (size_t i) const { return *(order_stream_[i]); }
+	
 	size_t size() const { return order_stream_.size(); }
+	
 	void parse(const char* filename);
 
-	~OrderBook()
+	//ctntodo (Only run for test) Method checking if the order book satisfied all conditions
+	bool check_goodness_order_book(){ return true; }
+
+	~InputOrderContainer()
 	{
 		for (size_t i = 0; i < order_stream_.size(); ++i)
 		{
@@ -71,9 +85,32 @@ public:
 		}
 	}
 private:
-	std::deque<const Order*> order_stream_;
+	std::vector<const Order*> order_stream_;
 };
-std::ostream& operator<<(std::ostream& os, const OrderBook& orderstream);
+std::ostream& operator<<(std::ostream& os, const InputOrderContainer& orderstream);
+
+class OrderBook
+{
+public:
+	
+	double highestPrice()const;
+		
+	double time_weighted_average() const;
+	
+	void erase(unsigned long erased_time, const unsigned int orderID);
+	void insert(unsigned long inserted_time, unsigned int orderID, const double& price);
+
+	friend std::ostream& operator<<(std::ostream& os, const OrderBook& orderbook);
+private:
+	
+	std::map<unsigned int, InsertOrder> m_order_book_;
+	
+	double time_average_buffer;
+	double highest_price_buffer;
+	double total_time_buffer;
+};
+
+
 
 
 int main(int argc, char ** argv)
@@ -83,9 +120,23 @@ int main(int argc, char ** argv)
 		std::cout << "Command need a file name as argument " << std::endl;  return 0;
 	}
 	
-	OrderStream order_stream;
-	order_stream.parse(argv[1]) ; //std::cout << order_stream << std::endl;
+	InputOrderContainer order_stream;
+	order_stream.parse(argv[1]); std::cout << order_stream << std::endl << std::endl;
 	
+	OrderBook order_book;
+	for (size_t i = 0; i < order_stream.size(); ++i)
+	{
+		const Order& order = order_stream[i];
+		if (order.isInsert())
+			order_book.insert(order.time,order.orderID,order.price());
+		else//is erase order
+			order_book.erase(order.time,order.orderID);
+
+		std::cout << "Highest Price=" << order_book.highestPrice()
+			<< "Time_Price Average=" << order_book.time_weighted_average() << std::endl
+			<< order_book << std::endl << std::endl;
+	}
+	/*
 	StreamReader stream_reader;
 
 	stream_reader.processOrderStream(order_stream);
@@ -93,11 +144,66 @@ int main(int argc, char ** argv)
 	std::cout << std::endl
 		<< " Highest Price " << stream_reader.maxPrice() << std::endl
 		<< " Time weighted average price " << stream_reader.timeAveragePrice() << std::endl;
-
+	*/
 	return 0;
 }
 
-void OrderBook::parse(const char* filename)
+
+double OrderBook::highestPrice()const
+{
+	if (m_order_book_.empty())
+		return std::numeric_limits<double>::quiet_NaN();
+	else
+		return highest_price_buffer;
+}
+
+double OrderBook::time_weighted_average() const
+{
+	if (m_order_book_.empty())
+		return std::numeric_limits<double>::quiet_NaN();
+	else
+		return time_average_buffer;
+}
+
+void OrderBook::erase(unsigned long erased_time, unsigned int orderID)
+{
+	assert(!m_order_book_.empty());
+	assert( erased_time > m_order_book_[orderID].time);
+	
+	const unsigned int order_duration = erased_time - m_order_book_[orderID].time;
+	const double order_price = m_order_book_[orderID].price();
+	
+	m_order_book_.erase(orderID);
+	
+	const double& old_total_time = total_time_buffer;
+	
+	// update time average price for all order
+	time_average_buffer *= old_total_time;
+	time_average_buffer += order_duration*order_price;
+
+	//update total time
+	total_time_buffer += order_duration;
+	time_average_buffer /= time_average_buffer;
+
+}
+void OrderBook::insert(unsigned long inserted_time, unsigned int orderID, const double& price)
+{
+	InsertOrder new_inserted_order(inserted_time, orderID, price);
+	m_order_book_[orderID] = new_inserted_order;
+
+	if (highest_price_buffer < price) highest_price_buffer = price;
+}
+
+
+double InputOrderContainer::highestPrice()const
+{
+	double highest_price = 0;
+
+
+	return highest_price;
+}
+
+void InputOrderContainer::parse(const char* filename)
 {
 	//std::cout <<" ============= Filename "<< filename << std::endl;
 	std::ifstream instream;
@@ -132,13 +238,13 @@ void OrderBook::parse(const char* filename)
 					assert(cell_array[1].at(0) == 'I');//Insert order
 					const double price = atof(cell_array[3].c_str());
 					Order* insertOrder = new InsertOrder(time, orderID, price);
-					this->push_back(insertOrder);
+					order_stream_.push_back(insertOrder);
 				}
 				else
 				{
 					assert(cell_array[1].at(0) == 'E' && cell_array.size() == 3);//Insert order
 					Order* eraseOrder = new EraseOrder(time, orderID);
-					this->push_back(eraseOrder);
+					order_stream_.push_back(eraseOrder);
 				}
 				
 			}
@@ -162,11 +268,23 @@ std::ostream& operator<<(std::ostream& os, const Order& order)
 	return os;
 }
 
-std::ostream& operator<<(std::ostream& os, const OrderBook& orderstream)
+std::ostream& operator<<(std::ostream& os, const InputOrderContainer& orderstream)
 {
 	for (size_t i = 0; i < orderstream.size(); ++i)
 	{
 		std::cout << orderstream[i]<<std::endl;
+	}
+	return os;
+}
+
+std::ostream& operator<<(std::ostream& os, const OrderBook& orderbook)
+{
+	std::map<unsigned int, InsertOrder>::const_iterator it = orderbook.m_order_book_.begin();
+	while (it != orderbook.m_order_book_.end())
+	{
+		const Order& order = it->second;
+		std::cout << order << std::endl;
+		++it;
 	}
 	return os;
 }
