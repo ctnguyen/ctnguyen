070c00ba09df95fb8d0fe07f6a6fcabd5b7752b1
chithanhnguyen.math <chithanhnguyen.math@gmail.com>
2015-03-07 21:47:11 +0000

    cpp Maxeler update first implementation of VMimage

--- ctn_cpp/tempo/Maxeler/unittest_VMimage.cpp
+++ ctn_cpp/tempo/Maxeler/unittest_VMimage.cpp
@@ -9,7 +9,15 @@ BOOST_AUTO_TEST_SUITE(test_VMimage)
 
 BOOST_AUTO_TEST_CASE(test_VMimage_Constructor)
 {
-	VMimage object;
+	VMimage data;
+	const std::string file_path_name = "test_data\\test01.bin";
+
+	data.load(file_path_name.c_str());
+
+	for (size_t i = 0; i < data.size(); ++i)
+	{
+		std::cout << data[i] << std::endl;
+	}
 
 	BOOST_CHECK(true);
 }
