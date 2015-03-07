5f0cfbfcef257e28917510bb8588232b3634c7a0
chithanhnguyen.math@gmail.com <chithanhnguyen.math@gmail.com@d822209c-5b66-c3cb-24dc-bd16cd606af5>
2015-03-07 22:54:00 +0000

    cpp Maxeler - finish task1

--- trunk/ctn_cpp/tempo/Maxeler/task1.cpp
+++ trunk/ctn_cpp/tempo/Maxeler/task1.cpp
@@ -4,7 +4,28 @@
 
 int main(int argc, char ** argv)
 {
-	VMimage object;
+	const char* file_path_name = "task1.bin";
+
+	if (argc == 2)
+	{
+		file_path_name = argv[1];
+	}
+
+	VMimage data;
+	
+	try
+	{
+		data.load(file_path_name);
+
+		for (size_t i = 0; i < (size_t)data.size(); ++i)
+		{
+			std::cout << data[i] << std::endl;
+		}
+	}
+	catch (std::string mgs)
+	{
+		std::cout << mgs << std::endl;
+	}
 
 	return 0;
 }
