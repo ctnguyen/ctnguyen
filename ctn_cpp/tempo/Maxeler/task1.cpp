67a7330644690020a1aa74f07b32487879c429bd
chithanhnguyen.math <chithanhnguyen.math@gmail.com>
2015-03-08 11:09:16 +0000

    cpp Maxeler - fully finished for task1

--- ctn_cpp/tempo/Maxeler/task1.cpp
+++ ctn_cpp/tempo/Maxeler/task1.cpp
@@ -5,6 +5,8 @@
 int main(int argc, char ** argv)
 {
 	const char* file_path_name = "task1.bin";
+	//const char* file_path_name = "test_data\\test01.bin";
+	//const char* file_path_name = "prog.ilk";
 
 	if (argc == 2)
 	{
@@ -17,10 +19,7 @@ int main(int argc, char ** argv)
 	{
 		data.load(file_path_name);
 
-		for (size_t i = 0; i < (size_t)data.size(); ++i)
-		{
-			std::cout << data[i] << std::endl;
-		}
+		std::cout << data << std::endl;
 	}
 	catch (std::string mgs)
 	{
