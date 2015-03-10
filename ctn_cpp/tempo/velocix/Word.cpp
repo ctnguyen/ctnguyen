ee367b12307c311ec444597ad4671526c2f8eb20
chithanhnguyen.math <chithanhnguyen.math@gmail.com>
2015-03-10 01:06:49 +0000

    cpp velocix - finished Word class

--- ctn_cpp/tempo/velocix/Word.cpp
+++ ctn_cpp/tempo/velocix/Word.cpp
@@ -1,8 +1,93 @@
 #include "Word.h"
 
+#include <cctype>
+#include <cassert>
+
+#define MAX_WORD_LENGHT 25
+
 Word::Word()
+	: text_(NULL)
 {
-	std::cout<<"Hello I am Word"<<std::endl;
+}
+
+Word::~Word()
+{
+	if (text_ != NULL)
+		delete[] text_;
+	text_ = NULL;
+}
+
+bool Word::empty() const
+{
+	return ((text_ == NULL));
+}
+
+const char* Word::get_text() const
+{
+	assert(text_ != NULL);
+	return text_;
+}
+
+bool Word::is_Symbol(const char& c) const
+{
+	if (isalpha(c)) return false;
+
+	return true;
+}
+
+std::ostream& operator<<(std::ostream &os, const Word& word)
+{
+	if (!word.empty())
+		os << "["<< word.get_text()<<"]";
+	return os;
+}
+
+std::istream& operator>>(std::istream &ifs, Word& word)
+{
+	// clear text_
+	// read the stream into a buffer (exluding all symbol, blank)
+	// newly allocated text word
+	// copy buffer to text word
+
+	// clear older text
+	if (word.text_ != NULL)
+	{
+		delete[] word.text_;
+		word.text_ = NULL;
+	}
+
+	unsigned int word_size = 0;
+
+	char text_buffer[MAX_WORD_LENGHT];
+
+	// read stream to buffer
+	while (ifs.good())
+	{
+		char c = ' ';
+		ifs.get(c);
+
+		if (word.is_Symbol(c)) break; // todo not only blank break, but all symbol character
+
+		text_buffer[word_size] = c;
+		++word_size;
+	}
+
+	if (word_size > 0)
+	{
+
+		text_buffer[word_size] = '\0';	++word_size;
+
+		// allocated new text word
+		word.text_ = new char[word_size];
+
+		// copy buffer to text word
+		for (size_t i = 0; i < word_size; ++i)
+		{
+			word.text_[i] = text_buffer[i];
+		}
+	}
+
+	return ifs;
 }
 
 
