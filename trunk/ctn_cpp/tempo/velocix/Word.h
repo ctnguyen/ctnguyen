c6ce8ecbc5990c77b4849d8b9c5dc2952ace2f1d
chithanhnguyen.math@gmail.com <chithanhnguyen.math@gmail.com@d822209c-5b66-c3cb-24dc-bd16cd606af5>
2015-03-10 01:06:49 +0000

    cpp velocix - finished Word class

--- trunk/ctn_cpp/tempo/velocix/Word.h
+++ trunk/ctn_cpp/tempo/velocix/Word.h
@@ -14,8 +14,27 @@
  */
 class Word
 {
-public :
+public:
+
 	Word();
+	~Word();
+
+	// check if word is empty
+	bool empty() const;
+
+	// get the text of the word
+	const char* get_text() const ;
+
+	// i\o operator
+	friend std::ostream& operator<<(std::ostream &os, const Word& word);
+	friend std::istream& operator>>(std::istream &is, Word& bignum);
+
+private :
+	// the text of the word
+	char* text_;
+
+	// check if a character is a symbol or a blank
+	bool is_Symbol(const char& c) const ;
 };
 
 #endif /* WORD_H */
--- trunk/ctn_cpp/tempo/velocix/unittest_Word.cpp
+++ trunk/ctn_cpp/tempo/velocix/unittest_Word.cpp
@@ -5,12 +5,31 @@
 #include "Helper.h"
 #include "Word.h"
 
+#include <ctype.h>
 
 BOOST_AUTO_TEST_SUITE(test_Word)
 
-BOOST_AUTO_TEST_CASE(test_constructor)
+BOOST_AUTO_TEST_CASE(test_reading_sample_text)
 {
-	Word m_word;
+	
+	const char* file_path_name = "sample_text.txt";
+
+	std::ifstream file_stream;
+	file_stream.open(file_path_name, std::ios::in);
+	
+	if (file_stream.is_open())
+	{
+		Word word;
+		while (file_stream.good() )
+		{
+			file_stream >> word;
+			if (!word.empty())
+				std::cout << word << std::endl;
+		}
+	}
+
+	file_stream.close();
+
 	BOOST_CHECK(true);
 }
 
