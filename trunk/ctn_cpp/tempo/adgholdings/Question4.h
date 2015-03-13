6a2be12bafbd2baba45da859cc57d594ebd5c5df
chithanhnguyen.math@gmail.com <chithanhnguyen.math@gmail.com@d822209c-5b66-c3cb-24dc-bd16cd606af5>
2015-03-13 17:00:42 +0000

    cpp adg holdings last commit from windows

--- trunk/ctn_cpp/tempo/adgholdings/Question4.h
+++ trunk/ctn_cpp/tempo/adgholdings/Question4.h
@@ -10,57 +10,85 @@
 
 namespace Q4
 {
-	int A[] = {1,-1,3,8,4};
-	//int A[] = {1,2,3,4,5};
+	//int A[] = {1,-1,3,8,4};  // 1+8=9
+	int A[] = { 1, 2, 3, 4, 5 };   //1+3+5=9
+	//int A[] = { 3, 2, 5, 10, 7 }; // 3+5+7
 
 	void GetMaxSum(int& sum, std::list<int>& res)
 	{
 		res.clear();
 		
-		int odd_running_sum=0;
-		int even_running_sum=0;
+		int incl = A[0];
+		int excl = 0;
 
-		const unsigned int lenght = (sizeof(A) / sizeof(*A));
+		const unsigned int LENGHT = (sizeof(A) / sizeof(*A));
 		
-		// even sum
-		for (size_t i = 0; i < lenght; i+=2)
+		// drop first non positive elements
+		size_t start_i = 0;
+		while (start_i < LENGHT && A[start_i] <= 0){ ++start_i;  }
+		
+		size_t parity_index = start_i;
+		bool is_even_used;
+		int n_excl = 0;
+		int n_incl = A[start_i];
+		std::list<int> odd_list;
+		std::list<int> event_list;
+		if (A[start_i] > 0)
 		{
-			if (A[i]>0)
-				even_running_sum += A[i];
-		}
-
-		// odd sum
-		for (size_t ii = 1; ii < lenght; ii += 2)
-		{
-			if (A[ii]>0)
-				odd_running_sum += A[ii];
-		}
-
-		// all the array are negatives
-		// the max negative number is the max sum
-		if (odd_running_sum == 0 && even_running_sum == 0)
-		{
-			sum = *std::max_element(A, A + lenght);			
-			res.push_back(sum);
-		}
-		else if (odd_running_sum > even_running_sum)
-		{
-			sum = odd_running_sum;
-			// push back the list			
-			for (size_t ii = 1; ii < lenght; ii += 2)
-			{
-				if (A[ii]>0) res.push_back(A[ii]);
+			if (start_i % 2 == 0) {
+				event_list.push_back(A[start_i]);
+				is_even_used = true;
+			}
+			else{
+				odd_list.push_back(A[start_i]);
+				is_even_used = false;
 			}
 		}
+		
+		++start_i;
+
+		size_t i = start_i;
+		while (i < LENGHT)
+		{
+			const int& current_value = A[i];
+			// save old values
+			const int o_incl = n_incl;
+			const int o_excl = n_excl;
+		
+			// compute the new max excluding current value
+			n_excl = (n_incl > n_excl) ? n_incl : n_excl;
+						
+			n_incl = (std::max)(o_excl + current_value, o_incl);
+
+			// normally if all values are positive, use i to count the parity
+			// But when a negative value occur, the parity has to be switched
+			// ctntodo if have more time , may be refactore this potential bug
+			if (current_value>0)
+				parity_index +=1;
+			else
+				parity_index +=2;
+
+			if (n_incl > n_excl)
+			{				
+				if (parity_index % 2 == 0){
+					event_list.push_back(current_value);
+					is_even_used = true;
+				}
+				else{
+					odd_list.push_back(current_value);
+					is_even_used = false;
+				}
+			}
+
+			++i;
+		}
+
+		sum = (n_incl > n_excl) ? n_incl : n_excl;
+
+		if (is_even_used)
+			res = event_list;
 		else
-		{
-			sum = even_running_sum;
-			// push back the list			
-			for (size_t i = 0; i < lenght; i += 2)
-			{
-				if (A[i]>0)	res.push_back(A[i]);
-			}
-		}
+			res = odd_list;
 	}
 
 
