0ea6325635e5375a138d60e1711dcf17cecb25b3
chithanhnguyen.math@gmail.com <chithanhnguyen.math@gmail.com@d822209c-5b66-c3cb-24dc-bd16cd606af5>
2015-03-13 12:46:53 +0000

    cpp adg holdings - finished question 3

--- trunk/ctn_cpp/tempo/adgholdings/unittest_Q1.cpp
+++ trunk/ctn_cpp/tempo/adgholdings/unittest_Q1.cpp
@@ -8,31 +8,6 @@
 #include "Question1.h"
 BOOST_AUTO_TEST_SUITE(test_Question1)
 
-BOOST_AUTO_TEST_CASE(test_constructor)
-{
-	Q1::Character mchar_a('a');
-	//std::cout << mchar_a << std::endl;//ctndebug
-	++mchar_a;
-	//std::cout << mchar_a << std::endl;//ctndebug
-	
-	Q1::Character mchar_b('B');
-	//std::cout << mchar_b << std::endl;//ctndebug
-	
-	BOOST_CHECK(true);
-}
-
-BOOST_AUTO_TEST_CASE(test_comparing_operator)
-{
-	Q1::Character chara('a');
-	Q1::Character charaa('a');
-	Q1::Character charb('b');
-	Q1::Character charz('z');
-	BOOST_CHECK(!(chara < charaa));
-	BOOST_CHECK(chara < charb);
-	BOOST_CHECK(chara < charz);
-	BOOST_CHECK(charb < charz);
-}
-
 BOOST_AUTO_TEST_CASE(test_function_q2)
 {
 	std::list<char> res;
