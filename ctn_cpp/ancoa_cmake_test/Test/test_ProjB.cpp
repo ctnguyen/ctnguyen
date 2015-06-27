3326783b641f941abfc7fba778fa8ab75394a5a7
Chi Thanh NGUYEN <chithanh.nguyen@ancoa.com>
2015-06-27 23:39:51 +0100

    [ancoa cmake] add test to project

--- ctn_cpp/ancoa_cmake_test/Test/test_ProjB.cpp
+++ ctn_cpp/ancoa_cmake_test/Test/test_ProjB.cpp
@@ -1,27 +1,14 @@
-/************************************************
-*     Mini Porject helloworld for cmake         *
-*     @authors Chi Thanh NGUYEN                 *
-*     contact chithanhnguyen.math@gmail.com     *
-*************************************************/
-/*!
- *	@file prog/say_hello.cpp
- *	@date Sept 2013
+/**
+ *	@file Test/test_Proj1.hpp
+ *	@date June 2015
  */
+#include <ProjB/ProjB.hpp>
+#define BOOST_TEST_MODULE testProjB
+#include <boost/test/unit_test.hpp>
 
-#include <hello.hpp>
-#include <modules/hello_english.hpp>
-#include <modules/hello_french.hpp>
-#include <modules/hello_vietnamese.hpp>
-
-int main()
+BOOST_AUTO_TEST_CASE( constructor )
 {
-	hello_english he;
-	hello_french hf;
-	hello_vietnamese hv;
+    ProjB projB;
 
-	he.say_hello();
-	hf.say_hello();
-	hv.say_hello();
-
-	return 0;
+    BOOST_CHECK(true);
 }
