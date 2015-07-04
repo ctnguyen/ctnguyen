21072cb32f863fa38475196f8f605dc13e8eca8a
Chi Thanh NGUYEN <chithanh.nguyen@ancoa.com>
2015-07-04 12:42:47 +0100

    [cpp] ancoa - Set postfix configuration build dependency x64, x86, debug or not . (-x46d for example) - Set target folders for all build (lib and exec), excepted tests program to be set after.

--- ctn_cpp/ancoa_cmake_test/cmake/SpecificBuildConfig.cmake
+++ ctn_cpp/ancoa_cmake_test/cmake/SpecificBuildConfig.cmake
@@ -1,13 +1,7 @@
-function(check64Bit resultHolder)
-
-	if( CMAKE_SIZEOF_VOID_P EQUAL 8 )
-		set(${resultHolder} ON PARENT_SCOPE) 
-	else()
-		set(${resultHolder} OFF PARENT_SCOPE) 
-	endif()
-
-endfunction()
-
+# Depending on a build of x64 or x86, give the path to 
+# ${CMAKE_BINARY_DIR}/x64 
+# ${CMAKE_BINARY_DIR}/x86
+# that should be used instead of ${CMAKE_BINARY_DIR}
 function(getRootBuildFolder resultPathHolder)
     
     set(pathRootBuildFolder "${CMAKE_BINARY_DIR}") # initialize path for build folder to binary directory
@@ -22,7 +16,6 @@ function(getRootBuildFolder resultPathHolder)
 endfunction()
 
 
-
 function(ctnSetTargetFolderProperties targetName)
 
     getRootBuildFolder(pathToRootBuildFolder)
@@ -33,4 +26,13 @@ function(ctnSetTargetFolderProperties targetName)
         set_target_properties( ${targetName} PROPERTIES LIBRARY_OUTPUT_DIRECTORY_${OUTPUTCONFIG} "${pathToRootBuildFolder}/${OUTPUTCONFIG}" )
         set_target_properties( ${targetName} PROPERTIES ARCHIVE_OUTPUT_DIRECTORY_${OUTPUTCONFIG} "${pathToRootBuildFolder}/${OUTPUTCONFIG}" )
     endforeach( OUTPUTCONFIG CMAKE_CONFIGURATION_TYPES )
+	
+	if( CMAKE_SIZEOF_VOID_P EQUAL 8 )
+		set(systemPostFix "-x64")
+	else()
+		set(systemPostFix "-x86")
+	endif()
+	
+	set_target_properties( ${targetName} PROPERTIES DEBUG_POSTFIX "${systemPostFix}d" )
+	
 endfunction()
