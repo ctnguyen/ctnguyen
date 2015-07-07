41cd09b39f208fd8547f7f3e49ababc9855e98f1
Chi Thanh NGUYEN <chithanh.nguyen@ancoa.com>
2015-07-07 10:53:23 +0100

    [cpp ancoa] add program to test linking helloworld with Qt.

---
+++ ctn_cpp/ancoa_cmake_test/ProgQt/ProgQt.cpp
@@ -0,0 +1,23 @@
+/************************************************
+*     Mini Porject for cmake test full feature  *
+*     Use of cmake, cTest, cPack, Dashboard     *
+*     @authors Chi Thanh NGUYEN                 *
+*     contact chithanhnguyen.math@gmail.com     *
+*************************************************/
+/*!
+ *	@file ProgQt/ProgQt.cpp
+ *	@date Jul 2015
+ */
+
+#include <QApplication>
+#include <QPushButton>
+ 
+int main(int argc, char **argv)
+{
+ QApplication app (argc, argv);
+ 
+ QPushButton button ("Hello world !");
+ button.show();
+ 
+ return app.exec();
+}
