ec15435c435b17cc8e5423fe53a644ccad734e7b
Chi Thanh NGUYEN <chithanh.nguyen@ancoa.com>
2015-07-07 18:18:45 +0100

    [cpp ancoa] First success compilation for linking with Qt. Need to see how to run.

--- ctn_cpp/ancoa_cmake_test/ProgQt/ProgQt.cpp
+++ ctn_cpp/ancoa_cmake_test/ProgQt/ProgQt.cpp
@@ -14,10 +14,10 @@
  
 int main(int argc, char **argv)
 {
- QApplication app (argc, argv);
- 
- QPushButton button ("Hello world !");
- button.show();
- 
- return app.exec();
+    QApplication app(argc, argv);
+
+    QPushButton button("Hello world !");
+    button.show();
+
+    return app.exec();
 }
