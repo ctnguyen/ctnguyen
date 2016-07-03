9f8d1a73bed435ee6477a2d257c285dcbff6f7a4
Chi Thanh NGUYEN <chithanh.nguyen@ancoa.com>
2016-07-03 11:52:45 +0100

    [cpp] add dynamic linking with boost unit test framework.

--- ctn_cpp/testBoost/test/test_Boost_Serialization/test_Boost_SerializationAllNativeType.cpp
+++ ctn_cpp/testBoost/test/test_Boost_Serialization/test_Boost_SerializationAllNativeType.cpp
@@ -1,5 +1,7 @@
-#include <iostream>
+#define BOOST_TEST_MODULE test_Boost_SerializationAllNativeType
+#include <boost/test/unit_test.hpp>
 
+#include <iostream>
 #include <cstddef> // NULL
 #include <iomanip>
 #include <iostream>
@@ -40,44 +42,43 @@ double
 
 void test_char_array_archive_string1()
 {
-    const size_t buffer_size = 50;
-    char buffer[buffer_size];
-    for(size_t i=0;i<buffer_size;++i)
-        buffer[i]='z';
+    //const size_t buffer_size = 50;
+    //char buffer[buffer_size];
+    //for(size_t i=0;i<buffer_size;++i)
+    //    buffer[i]='z';
 
-    boost::archive::archive_flags test_flag = boost::archive::no_header;
-    {
-        boost::iostreams::basic_array_sink<char> sr(buffer, buffer_size);
-        boost::iostreams::stream< boost::iostreams::basic_array_sink<char> > source(sr);
-        boost::archive::binary_oarchive oa(source, test_flag);
+    //boost::archive::archive_flags test_flag = boost::archive::no_header;
+    //{
+    //    boost::iostreams::basic_array_sink<char> sr(buffer, buffer_size);
+    //    boost::iostreams::stream< boost::iostreams::basic_array_sink<char> > source(sr);
+    //    boost::archive::binary_oarchive oa(source, test_flag);
 
-        const size_t sizeString = sizeof(std::string);
-        
-        /// first byte serialized for std::string is the number of characters
-        string1 string_wrapper;
-        string_wrapper.value="helloworlds";
-        oa << string_wrapper;
-    }
+    //    const size_t sizeString = sizeof(std::string);
+    //    
+    //    /// first byte serialized for std::string is the number of characters
+    //    string1 string_wrapper;
+    //    string_wrapper.value="helloworlds";
+    //    oa << string_wrapper;
+    //}
 
-    {
-        boost::iostreams::basic_array_source<char> sr(buffer, buffer_size);
-        boost::iostreams::stream< boost::iostreams::basic_array_source<char> > source(sr);
-        boost::archive::binary_iarchive oa(source, test_flag);
-        string1 string_wrapper;
-        oa >> string_wrapper;
-    }
-    const int abc=100;
+    //{
+    //    boost::iostreams::basic_array_source<char> sr(buffer, buffer_size);
+    //    boost::iostreams::stream< boost::iostreams::basic_array_source<char> > source(sr);
+    //    boost::archive::binary_iarchive oa(source, test_flag);
+    //    string1 string_wrapper;
+    //    oa >> string_wrapper;
+    //}
+    //const int abc=100;
 }
+BOOST_AUTO_TEST_SUITE(All_Types)
 
-
-
-int main(int argc, char *argv[])
+BOOST_AUTO_TEST_CASE(first_test)
 {
     //test_char_array_archive_stdstring();
     test_char_array_archive_string1();
     //test_char_array_archive_nativeCpp();
     //test_char_array_archive_customClasses();
-
-    const int i=100;
-    return 0;
+    BOOST_CHECK(true);
 }
+
+BOOST_AUTO_TEST_SUITE_END()
