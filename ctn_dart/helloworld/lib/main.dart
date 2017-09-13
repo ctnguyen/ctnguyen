45b0d1d93921134445a6635036e010cce41b380f
Chi Thanh NGUYEN <chithanh.nguyen@ancoa.com>
2017-09-13 15:50:56 +0100

    [dart] Add test websocket from dart connect to boost::beast.

--- ctn_dart/helloworld/lib/main.dart
+++ ctn_dart/helloworld/lib/main.dart
@@ -3,12 +3,27 @@ import 'package:flutter/material.dart';
 
 import 'helloworld_lib.dart';
 import 'test_LoginWidget.dart';
+import 'dart:io';
 
+/*
 void main() {
   //runApp(new TestHelloWorldApp());
+
   runApp(new MaterialApp(
       title: 'Flutter Tutorial',
       home: new InitWidget()
   ));
+
   //runApp(new Center(child: new Text('Hello, world!')));
+}
+*/
+
+// Example of websocket
+// Launch the phone emulator (tested on Android)
+// Run the example from boost::beast : ./websocket-server-sync 0.0.0.0 9090
+// Put the break point in the websocket_server_sync.cpp in funtion do_session to see if it connect
+// Run the project dart here.
+main() async {
+  var socket = await WebSocket.connect('ws://10.0.2.2:9090');
+  socket.add('Hello, World!');
 }
