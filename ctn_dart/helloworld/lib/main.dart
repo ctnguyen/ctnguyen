0bc1fcb9b9b1d96b940018b9a7a6488432aa8beb
Chi Thanh NGUYEN <chithanhnguyen.math@gmail.com>
2017-10-09 22:06:04 +0700

    Add comment showing how to configure and run on iOS simulator

--- ctn_dart/helloworld/lib/main.dart
+++ ctn_dart/helloworld/lib/main.dart
@@ -24,6 +24,12 @@ void main() {
 // Put the break point in the websocket_server_sync.cpp in funtion do_session to see if it connect
 // Run the project dart here.
 main() async {
-  var socket = await WebSocket.connect('ws://10.0.2.2:9090');
+  //var socket = await WebSocket.connect('ws://10.0.2.2:9090'); // For Android Simulator, run on 10.0.2.2 (windows)
+  //var socket = await WebSocket.connect('ws://localhost:9090');  // For iOS simulator, run on localhost
+
+  // For iOS simulator, run with server on linux virtual machine (Virtualbox).
+  // See how to access nat guest from host virtualbox (Configure port forwarding)
+  // http://ask.xmodulo.com/access-nat-guest-from-host-virtualbox.html
+  var socket = await WebSocket.connect('ws://localhost:9090');
   socket.add('Hello, World!');
 }
