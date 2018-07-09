4b59a0e8e113d6c1c335b2e91dc7fb4f01e8201d
Chi Thanh NGUYEN <chithanhnguyen.math@gmail.com>
2018-07-09 07:45:55 +0100

    Add test sizeof cpp.

--- ctn_cpp/test_cpp/test/unit/test_newcpp.cpp
+++ ctn_cpp/test_cpp/test/unit/test_newcpp.cpp
@@ -9,12 +9,40 @@
 #include <boost/test/unit_test.hpp>
 
 #include <iostream>
+#include <vector>
+#include <array>
 
 BOOST_AUTO_TEST_SUITE(test_all_newcpp)
 
-BOOST_AUTO_TEST_CASE(empty_test)
+struct EmptyStruct{};
+BOOST_AUTO_TEST_CASE(test_size_of)
 {
-    BOOST_CHECK(true);
+    const size_t size_of_bool = sizeof(bool);
+    const size_t size_of_char = sizeof(char);
+    const size_t size_of_empt = sizeof(EmptyStruct);
+    
+    BOOST_TEST(size_of_bool, size_of_char);
+    BOOST_TEST(size_of_empt, size_of_char);
+    
+    const size_t nbElem=10;
+    std::vector<bool> boolvect3;
+    std::vector<bool> boolvect4;
+    for(size_t i=0;i<nbElem;++i)
+        boolvect3.push_back(i%2==0);
+    for(size_t i=0;i<nbElem+1;++i)
+        boolvect4.push_back(!(i%2==0));
+    std::vector<bool> boolvect1{true};
+    std::vector<bool> boolvect2{true,false};
+    std::vector<bool> boolvect{true,true,false,false,true,false,true,false};
+    const size_t size_of_boolvect1 = sizeof boolvect1;//?
+    const size_t size_of_boolvect2 = sizeof boolvect2;//?
+    const size_t size_of_boolvect3 = sizeof boolvect3;//?
+    const size_t size_of_boolvect4 = sizeof boolvect4;//?
+    const size_t size_of_boolvect = sizeof(boolvect);//?
+    const size_t size_of_boolvecttype = sizeof(std::vector<bool>);
+    const size_t size_of_emptvect = sizeof(std::vector<EmptyStruct>);//?
+
+    const size_t size_of_array = sizeof(std::array<bool,10>);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
